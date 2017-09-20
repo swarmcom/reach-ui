36f9ed65e6769555821d79f3d1b2dd1c04566e24
Roman Galeev <roman.galeev@ezuce.com>
2017-09-20 12:54:58 +0200

    Imporve agents view a bit

--- src/Admin/Agents.vue
+++ src/Admin/Agents.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <div class="my-1 row">
-      <div class="col-md-9">
+    <div class="row">
+      <div class="col">
         <button @click="add" class="btn btn-outline-success"><icon name="plus" scale="1"></icon></button>
       </div>
-      <div class="col-md-1">
+      <div class="col-md-2 float-right">
         <b-form-select :options="pageOptions" v-model="perPage" />
       </div>
-      <div class="col-md-2">
-        <b-form-input v-model="filter" placeholder="Type to Search" />
+      <div class="col-md-4 float-right">
+        <b-form-input v-model="filter" placeholder="Type to Filter" />
       </div>
     </div>
-    <b-table striped hover small responsive
+    <b-table style="margin-top:10px" striped hover small responsive
       :current-page="currentPage"
       :per-page="perPage"
       :sort-by="sortBy"
-      :items="computedAgents"
+      :items="agents"
       :fields="fields"
       :filter="filter"
       :sortDesc="sortDesc"
@@ -33,38 +33,44 @@ export default {
   data () {
     return {
       fields: {
-        agent_id:     { label: 'Id', sortable: true },
-        login:      { label: 'Login', sortable: true  },
+        agent_id: { label: 'Id', sortable: true },
+        login: { label: 'Login', sortable: true  },
         firstname: { label: 'First Name', sortable: true  },
-        lastname:  { label: 'Last Name', sortable: true  },
+        lastname: { label: 'Last Name', sortable: true  },
         permissions: { label: 'Permissions', sortable: true  },
-        group_id: { label: 'Profile', sortable: true  },
+        group: { label: 'Group', sortable: true  },
         uri: { label: 'Uri', sortable: true  }
       },
       currentPage: 1,
-      pageOptions: [{text:'All', value:0},{text:5,value:5},{text:10,value:10},{text:15,value:15}, {text:20,value:20}, {text:25,value:25}, {text:30,value:30}],
+      pageOptions: [
+        {text:'All', value:0},
+        {text:5,value:5},{text:10,value:10},{text:15,value:15}, {text:20,value:20}, {text:25,value:25}, {text:30,value:30}
+      ],
       perPage: 0,
       filter: null,
       sortBy: 'agent_id',
       sortDesc: false,
       agents: [],
-      profiles: [],
+      groups: [],
       totalRows: 0
     }
   },
   methods: {
     query: async function () {
-      this.profiles = await this.$agent.p_mfa('ws_admin', 'get_profiles')
-      this.agents = await this.$agent.p_mfa('ws_admin', 'get_agents')
+      this.groups = await this.$agent.p_mfa('ws_admin', 'get_profiles')
+      let agents = await this.$agent.p_mfa('ws_admin', 'get_agents')
+      agents.forEach( (agent) => agent.group = this.group(agent.group_id) )
+      this.agents = agents
+      this.totalRows = agents.length
     },
     add () {
       this.$router.push(`/admin/agent/`)
     },
-    profile_name (Id) {
-      let Profile = this.profiles.find(I => I.id == Id)
-      return Profile ? Profile.name : ''
+    group (Id) {
+      let Group = this.groups.find(I => I.id == Id)
+      return Group ? Group.name : ''
     },
-    onClicked(data) {
+    onClicked (data) {
       this.$router.push(`/admin/agent/${data.agent_id}`)
     },
     onFiltered(filteredItems) {
@@ -76,17 +82,5 @@ export default {
   created () {
     this.query()
   },
-  computed: {
-    computedAgents () {
-      let agents = this.agents
-      let profiles = this.profiles
-      agents.forEach(function (key) {
-        let Profile = profiles.find(I => I.id == key.group_id)
-        Profile ? key.group_id = Profile.name : key.group_id = ''
-      })
-      this.totalRows = agents.length;
-      return agents;
-    }
-  }
 }
 </script>
