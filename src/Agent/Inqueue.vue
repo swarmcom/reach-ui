314e9bcd9c5fa5248c3c74332828a2fa6da7c9cc
Roman Galeev <roman.galeev@ezuce.com>
2017-10-04 13:01:09 +0200

    Add more conference controls

--- src/Agent/Inqueue.vue
+++ src/Agent/Inqueue.vue
@@ -1,26 +1,36 @@
 <template>
-<call v-if="call_visible" :uuid="uuid"></call>
+<div>
+  <call v-if="call_visible" :uuid="uuid"></call>
+  <conference v-if="conf_visible"></conference>
+</div>
 </template>
 
 <script>
 import Call from './Inqueue/Call'
+import Conference from './Inqueue/Conference'
 
 export default {
   data () {
     return {
       call_visible: false,
+      conf_visible: false,
       uuid: undefined
     }
   },
   methods: {
     handleState ({ state }) {
+      console.log('state', state)
       if (state.state == "available" || state.state == "release") {
         this.call_visible = false
+        this.conf_visible = false
         this.uuid = undefined
       } else {
         if (state.inqueue.inqueue_call) {
           this.call_visible = true
           this.uuid = state.inqueue.inqueue_call
+        }
+        if (state.state == 'conference') {
+          this.conf_visible = true
         }
       }
     }
@@ -32,7 +42,8 @@ export default {
     this.$bus.$off('agent_state', this.handleState)
   },
   components: {
-    call: Call
+    call: Call,
+    conference: Conference
   },
 }
 </script>
--- src/Agent/Inqueue/Conference.vue
+++ src/Agent/Inqueue/Conference.vue
@@ -1,6 +1,34 @@
 <template>
-<div v-if="visible">
-  <h1>Conference controls</h1>
+<div class="container" v-if="visible">
+<h3>Conference call:</h3>
+<div class="row" style="margin-top: 10px">
+  <div class="col">
+    <b-button size="sm" variant="outline-success" @click="kick('all')">Kick All</b-button>
+    <b-button size="sm" variant="outline-success" @click="mute('all')">Mute All</b-button>
+    <b-button size="sm" variant="outline-success" @click="unmute('all')">Unmute All</b-button>
+    <b-button size="sm" variant="outline-success" @click="deaf('all')">Deaf All</b-button>
+    <b-button size="sm" variant="outline-success" @click="undeaf('all')">Undeaf All</b-button>
+  </div>
+  <div class="col">
+    <b-button size="sm" variant="outline-warning" @click="inqueue()">Invite Inqueue</b-button>
+  </div>
+</div>
+<div class="row" style="margin-top: 10px" v-for="(v,k) in members">
+  <div class="col">
+    "{{v['Caller-Caller-ID-Name']}}" &lt;{{v['Caller-Caller-ID-Number']}}&gt;
+  </div>
+  <div class="col-1">
+    <b-button size="sm" variant="outline-primary" @click="kick(k)">Kick</b-button>
+  </div>
+  <div class="col-1">
+    <b-button v-if="v.Speak == 'true'" size="sm" variant="outline-primary" @click="mute(k)">Mute</b-button>
+    <b-button v-else size="sm" variant="outline-warning" @click="unmute(k)">Unmute</b-button>
+  </div>
+  <div class="col-1">
+    <b-button v-if="v.Hear == 'true'" size="sm" variant="outline-primary" @click="deaf(k)">Deaf</b-button>
+    <b-button v-else size="sm" variant="outline-warning" @click="undeaf(k)">Undeaf</b-button>
+  </div>
+</div>
 </div>
 </template>
 
@@ -8,25 +36,47 @@
 export default {
   data () {
     return {
+      members: {},
       visible: false
     }
   },
   methods: {
-    handleState ({ info, inqueue, call_info }) {
-      if (info.state == 'conference') {
-        this.visible = true
+    query: async function () {
+      this.members = await this.$agent.p_call('conference_members', [])
+      this.visible = true
+    },
+    handleConf ({action, member_id, vars}) {
+      if (action == "del-member") {
+        this.$delete(this.members, member_id)
       } else {
-        this.visible = false
+        this.$set(this.members, member_id, vars)
       }
     },
-    onTimer () {
+    kick (id) {
+      this.$agent.p_call('conference', ['kick', id])
     },
+    mute (id) {
+      this.$agent.p_call('conference', ['mute', id])
+    },
+    deaf (id) {
+      this.$agent.p_call('conference', ['deaf', id])
+    },
+    unmute (id) {
+      this.$agent.p_call('conference', ['unmute', id])
+    },
+    undeaf (id) {
+      this.$agent.p_call('conference', ['undeaf', id])
+    },
+    inqueue () {
+      this.$agent.p_call('conference', ['inqueue'])
+    }
   },
   created () {
-    this.$bus.$on('agent_state', (S) => this.handleState(S))
+    this.query()
+    this.$bus.$on('agent_conf', this.handleConf)
   },
-  mounted () {
-    this.onTimer()
-  }
+  beforeDestroy () {
+    this.$bus.$off('agent_conf', this.handleConf)
+  },
 }
 </script>
