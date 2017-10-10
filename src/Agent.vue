95eed0af6097250de2f141cdef65275cb12271ca
Roman Galeev <roman.galeev@ezuce.com>
2017-10-10 17:14:07 +0200

    Allow agent to temporarely override URI

--- src/Agent.vue
+++ src/Agent.vue
@@ -12,6 +12,7 @@
           <b-row class="col-10">
             <b-col cols="8">
               <dialer v-if="this.$agent.is_idle()"></dialer>
+              <override></override>
             </b-col>
             <b-col cols="12">
               <inqueue></inqueue>
@@ -40,6 +41,7 @@
 import ToggleBar from './Widget/ToggleBar'
 import Inqueue from './Agent/Inqueue'
 import Dialer from './Agent/Dialer'
+import Override from './Agent/Override'
 import Release from './Agent/Widget/Release'
 import AgentState from './Agent/AgentState'
 import AgentInfo from './Agent/Info'
@@ -65,7 +67,8 @@ export default {
     release: Release,
     toggleBar: ToggleBar,
     agentState: AgentState,
-    'agent-info': AgentInfo
+    'agent-info': AgentInfo,
+    override: Override
   },
 }
 </script>
