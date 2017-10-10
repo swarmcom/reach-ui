b8da2c2f7eddc28dcd409ca28c712a837d287c32
Roman Galeev <roman.galeev@ezuce.com>
2017-10-10 22:37:40 +0200

    Add agent call spy display and actions

--- src/agent.js
+++ src/agent.js
@@ -120,6 +120,7 @@ export default class Agent extends WsProto {
   is_wrapup () { return this.vm && this.vm.state == 'wrapup' }
   is_hold () { return this.vm && this.vm.state == 'hold' }
   is_onsession() { return this.vm && ( this.vm.state == 'oncall' || this.vm.state == 'conference' || this.vm.state == 'inconference' ) }
+  is_barge () { return this.vm && this.vm.state == 'barge' }
   can_call () { return this.vm && this.vm.agent.line_id && this.vm.agent.line_id != "undefined"}
   can_hangup () { return this.vm && ( this.vm.state == 'oncall' || this.vm.state == 'ringing' || this.vm.state == 'conference' || this.vm.state == 'inconference' ) }
   can_conference () { return this.vm && ( this.vm.state == 'oncall' || this.vm.state == 'conference' ) }
