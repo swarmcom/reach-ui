9898ce28f55798b5f7423bfd6b4eb4965ef0494d
Roman Galeev <roman.galeev@ezuce.com>
2017-10-10 09:26:59 +0200

    Outgoing and ringer displays

--- src/Agent/Inqueue/Outgoing.vue
+++ src/Agent/Inqueue/Outgoing.vue
@@ -1,8 +1,5 @@
 <template>
-<div v-if="this.outgoing" style="margin-top: 10px">
-  <div v-if="this.state == 'agent_leg'">
-    <h6>Calling: self</h6>
-  </div>
+<div style="margin-top: 20px">
   <div v-if="this.state == 'target_leg'">
     <h6>Calling: {{ target }}</h6>
   </div>
@@ -16,38 +13,11 @@
 export default {
   data () {
     return {
-      outgoing: false,
-      target: '',
-      state: ''
     }
   },
   methods: {
-    handleState ({ info, inqueue, call_info }) {
-      if (info.state == 'ringing' && info.inqueue.outgoing) {
-        this.target = info.inqueue.outgoing
-        this.state = 'agent_leg'
-        this.outgoing = true
-      } else if (info.state == 'outgoing') {
-        this.state = 'target_leg'
-      } else if (info.state == 'oncall') {
-        this.state = 'oncall'
-      } else if (info.state == 'hold') {
-      } else {
-        this.state = ''
-        this.target = ''
-        this.outgoing = false
-      }
-    },
-    onTimer () {
-    },
     hold () { this.$agent.hold() },
     unhold () { this.$agent.unhold() }
   },
-  created () {
-    this.$bus.$on('agent_state', (S) => this.handleState(S))
-  },
-  mounted () {
-    this.onTimer()
-  }
 }
 </script>
