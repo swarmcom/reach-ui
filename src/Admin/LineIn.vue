0ff3f94482bd7126fbb759f8e3ee42032e65e08f
Roman Galeev <roman.galeev@ezuce.com>
2017-10-12 00:28:35 +0200

    Allow to select voice prompts for Line In

--- src/Admin/LineIn.vue
+++ src/Admin/LineIn.vue
@@ -5,7 +5,9 @@
   <form-text id="description" label="Description" v-model="line_in.description"></form-text>
   <queues label="Queue" v-model="line_in.queue_id"></queues>
   <clients label="Client" v-model="line_in.client_id"></clients>
+  <prompts label="Announce" v-model="line_in.announce_id"></prompts>
   <form-select-bool label="Allow Voicemail" v-model="line_in.allow_voicemail"></form-select-bool>
+  <prompts label="Voicemail prompt" v-model="line_in.voicemail_prompt_id"></prompts>
   <button @click="onCommit" class="btn btn-primary">Commit</button>
   <button @click="onDelete" class="btn btn-danger float-right">Delete</button>
 </div>
@@ -16,12 +18,13 @@ import FormText from '../Widget/FormText.vue'
 import FormSelectBool from '../Widget/FormSelectBool.vue'
 import Queues from '../Widget/Queues.vue'
 import Clients from '../Widget/Clients.vue'
+import Prompts from '../Widget/Prompts.vue'
 import Common from './Common'
 
 export default {
   name: 'admin-line-in',
   props: ['id'],
-  components: { 'form-select-bool': FormSelectBool, 'form-text': FormText, 'queues': Queues, 'clients': Clients },
+  components: { 'form-select-bool': FormSelectBool, 'form-text': FormText, 'queues': Queues, 'clients': Clients, prompts: Prompts },
   mixins: [Common],
   data () {
     return {
---
+++ src/Widget/Prompts.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="form-group row">
+  <label class="col-3 col-form-label">{{ label }}</label>
+  <div class="col-9">
+    <select class="custom-select" :value="value" @change="onUpdate($event.target.value)">
+      <option></option>
+      <option v-for="prompt in prompts" :value="prompt.id" :selected="isActive(prompt.id)">{{ prompt.name }}</option>
+    </select>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'widget-prompts',
+  props: ['label', 'value'],
+  data () {
+    return {
+      prompts: []
+    }
+  },
+  methods: {
+    isActive(Id) {
+      return Id == this.value
+    },
+    query: async function () {
+      this.prompts = await this.$agent.p_mfa('ws_admin', 'get_prompts')
+    },
+    onUpdate (value) {
+      this.$emit('input', value)
+    }
+  },
+  created () {
+    this.query()
+  }
+}
+</script>
