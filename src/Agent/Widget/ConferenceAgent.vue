33cd5e561afc2e10678c9e6356df53dedab249e3
mharcar <martin.harcar@ezuce.com>
2017-10-11 16:59:14 +0200

    Disable transfer/conference in case 0 agents available and show hover message (#57)
    * Empty Drop Down when no other agents logged in
    
    * Disable transfer/conference in case 0 agents available and show hover message

--- src/Agent/Widget/ConferenceAgent.vue
+++ src/Agent/Widget/ConferenceAgent.vue
@@ -1,6 +1,8 @@
 <template>
-<b-dropdown text="Agent" variant="outline-primary">
-  <b-dropdown-item v-for="agent in this.$agent.vm.transfer_agents" :key="agent.id" @click="onUpdate(agent.id)">{{ agent.login }}</b-dropdown-item>
+<b-dropdown text="Agent" variant="outline-primary" :disabled=disable() :title=hover_message @mouseover.native="mouseOver">
+  <div>
+    <b-dropdown-item v-for="agent in this.$agent.vm.transfer_agents" :key="agent.id" @click="onUpdate(agent.id)">{{ agent.login }}</b-dropdown-item>
+  </div>
 </b-dropdown>
 </template>
 
@@ -8,11 +10,27 @@
 export default {
   name: 'conference-agent',
   data () {
-    return {}
+    return {
+      hover_message: '',
+      active: false
+    }
   },
   methods: {
     onUpdate (agent_id) {
       this.$agent.conference_to_agent(agent_id)
+    },
+    mouseOver: function(){
+      if (this.$agent.vm.transfer_agents.length > 0)
+        this.hover_message = ''
+      else
+        this.hover_message = 'No Other Agents'
+    },
+    disable: function () {
+      if (this.$agent.vm.transfer_agents.length > 0)
+        return false
+      else {
+        return true
+      }
     }
   }
 }
--- src/Agent/Widget/TransferAgent.vue
+++ src/Agent/Widget/TransferAgent.vue
@@ -1,5 +1,5 @@
 <template>
-<b-dropdown text="Agent" variant="outline-primary">
+<b-dropdown text="Agent" variant="outline-primary" :disabled=disable() :title=hover_message @mouseover.native="mouseOver">
   <b-dropdown-item v-for="agent in this.$agent.vm.transfer_agents" :key="agent.id" @click="onUpdate(agent.id)">{{ agent.login }}</b-dropdown-item>
 </b-dropdown>
 </template>
@@ -8,11 +8,27 @@
 export default {
   name: 'transfer-agent',
   data () {
-    return {}
+    return {
+      hover_message: '',
+      active: false
+    }
   },
   methods: {
     onUpdate (agent_id) {
       this.$agent.transfer_to_agent(agent_id)
+    },
+    mouseOver: function(){
+      if (this.$agent.vm.transfer_agents.length > 0)
+        this.hover_message = ''
+      else
+        this.hover_message = 'No Other Agents'
+    },
+    disable: function () {
+      if (this.$agent.vm.transfer_agents.length > 0)
+        return false
+      else {
+        return true
+      }
     }
   }
 }
