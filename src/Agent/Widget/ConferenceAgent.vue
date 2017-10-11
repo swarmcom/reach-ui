ce9fbb80f2fa05b4e23b2382ed39a7265bd93cc5
martin harcar <martin.harcar@ezuce.com>
2017-10-11 16:07:23 +0200

    Disable transfer/conference in case 0 agents available and show hover message

--- src/Agent/Widget/ConferenceAgent.vue
+++ src/Agent/Widget/ConferenceAgent.vue
@@ -1,10 +1,7 @@
 <template>
-<b-dropdown text="Agent" variant="outline-primary">
-  <div v-if="this.$agent.vm.transfer_agents.length > 0">
+<b-dropdown text="Agent" variant="outline-primary" :disabled=disable() :title=hover_message @mouseover.native="mouseOver">
+  <div>
     <b-dropdown-item v-for="agent in this.$agent.vm.transfer_agents" :key="agent.id" @click="onUpdate(agent.id)">{{ agent.login }}</b-dropdown-item>
-  </div>
-  <div v-else>
-    <b-dropdown-item>No Other Agents</b-dropdown-item>
   </div>
 </b-dropdown>
 </template>
@@ -13,11 +10,27 @@
 export default {
   name: 'conference-agent',
   data () {
-    return {}
+    return {
+      hover_message: '',
+      active: false
+    }
   },
   methods: {
     onUpdate (agent_id) {
       this.$agent.conference_to_agent(agent_id)
+    },
+    mouseOver: function(){
+      if (this.$agent.vm.transfer_agents.length > 0)
+        this.hover_message = ''
+      else
+        this.hover_message = 'No Other Agents'
+    },
+    disable: function () {
+      if (this.$agent.vm.transfer_agents.length > 0)
+        return false
+      else {
+        return true
+      }
     }
   }
 }
--- src/Agent/Widget/TransferAgent.vue
+++ src/Agent/Widget/TransferAgent.vue
@@ -1,10 +1,6 @@
 <template>
-<b-dropdown text="Agent" variant="outline-primary">
-  <div v-if="this.$agent.vm.transfer_agents.length > 0">
-    <b-dropdown-item v-for="agent in this.$agent.vm.transfer_agents" :key="agent.id" @click="onUpdate(agent.id)">{{ agent.login }}</b-dropdown-item>
-  <div v-else>
-    <b-dropdown-item>No Other Agents</b-dropdown-item>
-  </div>
+<b-dropdown text="Agent" variant="outline-primary" :disabled=disable() :title=hover_message @mouseover.native="mouseOver">
+  <b-dropdown-item v-for="agent in this.$agent.vm.transfer_agents" :key="agent.id" @click="onUpdate(agent.id)">{{ agent.login }}</b-dropdown-item>
 </b-dropdown>
 </template>
 
@@ -12,11 +8,27 @@
 export default {
   name: 'transfer-agent',
   data () {
-    return {}
+    return {
+      hover_message: '',
+      active: false
+    }
   },
   methods: {
     onUpdate (agent_id) {
       this.$agent.transfer_to_agent(agent_id)
+    },
+    mouseOver: function(){
+      if (this.$agent.vm.transfer_agents.length > 0)
+        this.hover_message = ''
+      else
+        this.hover_message = 'No Other Agents'
+    },
+    disable: function () {
+      if (this.$agent.vm.transfer_agents.length > 0)
+        return false
+      else {
+        return true
+      }
     }
   }
 }
