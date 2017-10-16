837787db2efae62ecd0f9399d049f9a861e59c34
Roman Galeev <roman.galeev@ezuce.com>
2017-10-16 11:48:34 +0200

    Add dispositio/release group/entries editors

--- src/Admin/DispositionEntries.vue
+++ src/Admin/DispositionEntries.vue
@@ -1,38 +1,76 @@
 <template>
-<btable :fields="fields" :data="dispositions" :add_button=true></btable>
+<div class="container">
+  <div class="row" style="margin-bottom: 10px"><div class="col"><h3>Dispositions:</h3></div></div>
+
+  <div v-for="disposition of dispositions" class="form-row">
+    <div class="for_group col-1">
+      <button @click="del(disposition.id)" class="btn btn-outline-danger"><icon name="minus" scale="1"></icon></button>
+    </div>
+    <div class="form-group col-4">
+      <input type="text" class="form-control"
+        :value="safe_value(disposition.name)" v-on:change="onChange(disposition, 'name', $event.target.value)">
+    </div>
+    <div class="form-group col-5">
+      <input type="text" class="form-control"
+        :value="safe_value(disposition.description)" v-on:change="onChange(disposition, 'description', $event.target.value)">
+    </div>
+  </div>
+
+  <div class="form-row">
+    <div class="form-group col-1">
+      <button @click="add" class="btn btn-outline-secondary"><icon name="plus" scale="1"></icon></button>
+    </div>
+    <div class="form-group col-4">
+      <input type="text" class="form-control" v-model="name" placeholder="Name">
+    </div>
+    <div class="form-group col-5">
+      <input type="text" class="form-control" v-model="description" placeholder="Description">
+    </div>
+  </div>
+
+</div>
 </template>
 
 <script>
-import Btable from '../Widget/Btable'
-
 export default {
   name: 'admin-disposition-entries',
+  props: ['id'],
   data () {
     return {
-      fields: {
-        id: { label: 'Id', sortable: true },
-        name: { label: 'Name', sortable: true },
-        description: { label: 'Description' }
-      },
+      name: undefined,
+      description: undefined,
       dispositions: []
     }
   },
   methods: {
     query: async function () {
-      this.dispositions = await this.$agent.p_mfa('ws_db_disposition', 'get')
+      this.dispositions = await this.$agent.p_mfa('ws_db_disposition', 'get', [this.id])
     },
-    add () {
-      this.$router.push(`/admin/disposition/`)
+    onChange (disposition, field, value) {
+      disposition[field] = value
+      this.$agent.p_mfa('ws_db_disposition', 'update', [disposition.id, disposition])
     },
-    onClick (data) {
-      this.$router.push(`/admin/disposition/${data.id}`)
+    add: async function() {
+      let disposition = await this.$agent.p_mfa('ws_db_disposition', 'create', [this.id, {
+        name: this.name, description: this.description
+      }])
+      this.dispositions.push(disposition)
+      this.name = undefined
+      this.description = undefined
     },
+    del: async function (Key) {
+      await this.$agent.p_mfa('ws_db_disposition', 'delete', [Key])
+      let id = this.dispositions.findIndex(Obj => Obj.id === Key)
+      if (id >= 0) {
+        this.dispositions.splice(id, 1)
+      }
+    },
+    safe_value (Value) {
+      return Value === 'undefined' ? '' : Value
+    }
   },
   created () {
     this.query()
-  },
-  components: {
-    btable: Btable
   }
 }
 </script>
