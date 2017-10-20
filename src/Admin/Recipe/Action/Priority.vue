94f0c76210e97472549eb440b254cfee6d545946
Roman Galeev <roman.galeev@ezuce.com>
2017-10-20 14:51:33 +0200

    Add more actions to recipe editor

---
+++ src/Admin/Recipe/Action/Priority.vue
@@ -0,0 +1,29 @@
+<template>
+<b-form-row>
+  <b-col>
+    <b-form-input v-model="value"></b-form-input>
+  </b-col>
+</b-form-row>
+</template>
+
+<script>
+export default {
+  name: 'admin-recipe-action-priority',
+  props: ['args'],
+  data () {
+    return {
+      value: this.args[0]
+    }
+  },
+  methods: {
+    commit () {
+      this.$emit('input', [this.value])
+    }
+  },
+  watch: {
+    value () {
+      this.commit()
+    }
+  },
+}
+</script>
