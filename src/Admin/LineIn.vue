613b245c002fa103d91d9eed85ff65095635802b
Roman Galeev <roman.galeev@ezuce.com>
2017-10-31 12:22:57 +0100

    Rename parameter

--- src/Admin/LineIn.vue
+++ src/Admin/LineIn.vue
@@ -6,7 +6,7 @@
   <luas label="Lua script" v-model="rec.lua_id"></luas>
   <queues label="Queue" v-model="rec.queue_id"></queues>
   <clients label="Client" v-model="rec.client_id"></clients>
-  <prompts label="Announce" v-model="rec.announce_id"></prompts>
+  <prompts label="Welcome Message" v-model="rec.announce_id"></prompts>
   <form-select-bool label="Allow Voicemail" v-model="rec.allow_voicemail"></form-select-bool>
   <form-select-bool label="Enable call recording" v-model="rec.enable_call_recording"></form-select-bool>
   <prompts label="Voicemail Prompt" v-model="rec.voicemail_prompt_id"></prompts>
