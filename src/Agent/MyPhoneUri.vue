0d2f4b595738b26f0c722a90f482dc7a8dc5e642
martin harcar <martin.harcar@ezuce.com>
2017-11-02 14:24:31 +0100

    Change myPhoneUri component

--- src/Agent/MyPhoneUri.vue
+++ src/Agent/MyPhoneUri.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
-    <div class="row col-12">
-      <b-input-group-button>
-        <b-form-input size="sm" variant="outline-secondary" v-model="uri" type="text"></b-form-input>
+  <div class="col-12">
+    <b-input-group>
+      <b-form-input variant="outline" size="sm" v-model="uri" type="text"></b-form-input>
+      <b-input-group-button size="sm">
         <b-btn size="sm" @click="override" variant="outline-secondary">Set</b-btn>
+        <b-dropdown size="sm" text="Contacts" variant="outline-secondary" right>
+          <b-dropdown-item v-for="uri of this.$agent.vm.agent.uris" :key="uri.uri" @click="set(uri.uri)">{{uri.uri}}</b-dropdown-item>
+        </b-dropdown>
+        <b-btn size="sm" @click="test" variant="outline-secondary">Test</b-btn>
       </b-input-group-button>
-    </div>
-    <div class="row col-12" style="margin-top:5px">
-      <b-dropdown size="sm" text="Contacts" right>
-        <b-dropdown-item v-for="uri of this.$agent.vm.agent.uris" :key="uri.uri" @click="set(uri.uri)">{{uri.uri}}</b-dropdown-item>
-      </b-dropdown>
-      <b-btn size="sm" style="margin-top:5px" @click="test" variant="outline-secondary">Test</b-btn>
-    </div>
+    </b-input-group>
   </div>
 </template>
 
