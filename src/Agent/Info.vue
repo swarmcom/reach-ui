63eaed0c717c43bfb58c96b0395d453826f97ce3
martin harcar <martin.harcar@ezuce.com>
2017-11-07 08:08:21 +0100

    Add profile name in my profile section

--- src/Agent/Info.vue
+++ src/Agent/Info.vue
@@ -31,7 +31,10 @@
       <div class="col-5 agent-state-text">Persistent:</div>
       <div class="col-7 agent-state-text"> {{ agent.persistent || '&nbsp' }} </div>
     </div>
-
+    <div class="row">
+      <div class="col-5 agent-state-text">Profile:</div>
+      <div class="col-7 agent-state-text"> {{ agent.group.name }} </div>
+    </div>
     <div class="row col-5 agent-state-text">Skills:</div>
     <div class="row" v-for="(v, k) in agent.skills">
       <dd class="col-5 agent-state-text"></dd>
