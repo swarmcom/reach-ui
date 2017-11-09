9aae56d38a0343942ec43656362f2271a8dd94a6
martin harcar <martin.harcar@ezuce.com>
2017-11-09 15:19:14 +0100

    Add my statistics component with hard coded statistics data

--- src/Agent.vue
+++ src/Agent.vue
@@ -3,20 +3,24 @@
   <b-row>
     <b-col><session-manager></session-manager></b-col>
   </b-row>
+  <b-row>
+    <b-col><my-statistics></my-statistics></b-col>
+  </b-row>
 </div>
 
 </template>
 
 <script>
 import SessionManager from './Agent/SessionManager.vue'
-
+import MyStatistics from './Agent/MyStatistics.vue'
 export default {
   data () {
     return {
     }
   },
   components: {
-    'session-manager': SessionManager
+    'session-manager': SessionManager,
+    'my-statistics': MyStatistics
   }
 }
 </script>
