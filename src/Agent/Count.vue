2fdc2cd5acdff9e946e838a5a3b2f9d56e03bb98
Roman Galeev <roman.galeev@ezuce.com>
2017-11-10 13:49:16 +0100

    Add agents states widget

---
+++ src/Agent/Count.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <h3>Agents states:</h3>
+  <b-row v-for="(count, state) in states" :key="state">
+    <b-col>{{state}}:</b-col><b-col>{{count}}</b-col>
+  </b-row>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'agent-count',
+  data () {
+    return {
+      states: {}
+    }
+  },
+  methods: {
+    query: async function () {
+      this.states = await this.$agent.p_mfa('ws_stats', 'agents_states', [])
+    },
+  },
+  created () {
+    this.query()
+  },
+}
+</script>
