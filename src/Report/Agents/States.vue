db417ebb0b6626eaa8280ca7d146d769a02777f1
Roman Galeev <roman.galeev@ezuce.com>
2017-11-22 18:44:50 +0100

    Abstract common query param widget

--- src/Report/Agents/States.vue
+++ src/Report/Agents/States.vue
@@ -1,67 +1,42 @@
 <template>
 <div>
-  <b-row style="margin-bottom: 10px">
-    <b-col cols=3>
-      <widget-date v-model="date_start" placeholder="Start date"></widget-date>
-    </b-col>
-    <b-col cols=3>
-      <widget-date v-model="date_end" placeholder="End date"></widget-date>
-    </b-col>
-    <b-col>
-      <widget-agent-groups v-model="groups"></widget-agent-groups>
-    </b-col>
-    <b-col cols=2>
-      <button @click="filter" class="btn btn-outline-primary">Apply</button>
-      <button @click="reset" class="btn btn-outline-success float-right">Reset</button>
-    </b-col>
+  <b-row>
+    <b-col><h2>Agents states man-hours</h2></b-col>
   </b-row>
+  <widget-query v-model="query_params" style="margin-top: 10px"></widget-query>
   <widget-chart v-model="re"></widget-chart>
 </div>
 </template>
 
 <script>
 import Chart from '@/Report/Widget/Chart'
-import ReportDate from '@/Report/Widget/Date'
-import AgentGroups from '@/Report/Widget/AgentGroups'
+import Query from '@/Report/Widget/Query'
 
 export default {
   name: 'report-agents-states',
   components: {
-    'widget-agent-groups': AgentGroups,
-    'widget-date': ReportDate,
-    'widget-chart': Chart
+    'widget-chart': Chart,
+    'widget-query': Query
   },
   data () {
     return {
       re: {},
-      date_start: undefined,
-      date_end: undefined,
-      groups: []
+      query_params: {}
     }
   },
   methods: {
-    reset () {
-      this.date_start = undefined,
-      this.date_end = undefined,
-      this.groups = [],
-      this.query()
-    },
-    filter () {
-      this.query()
-    },
-    make_query() {
-      return {
-        date_start: this.date_start,
-        date_end: this.date_end,
-        groups: this.groups
-      }
-    },
-    query: async function() {
-      this.re = await this.$agent.p_mfa('ws_report', 'agents_states', [this.make_query()])
+    query: async function(params) {
+      this.re = await this.$agent.p_mfa('ws_report', 'agents_states', [params])
     }
   },
   created () {
-    this.query()
+    this.query(this.query_params)
+  },
+  watch: {
+    query_params (value) {
+      this.query(value)
+      return value
+    }
   }
 }
 </script>
