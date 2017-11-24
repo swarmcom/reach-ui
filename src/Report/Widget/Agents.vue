f875e40e20b093f70a791e5407073c4f789a5017
Roman Galeev <roman.galeev@ezuce.com>
2017-11-24 14:14:30 +0100

    Add agents filter to reports

--- src/Report/Widget/Agents.vue
+++ src/Report/Widget/Agents.vue
@@ -1,33 +1,33 @@
 <template>
 <div class="form-inline">
-<autocomplete-agent-group v-model="group" placeholder="Agent group..."></autocomplete-agent-group>
-<button class="btn btn-outline-secondary" style="margin-left: 10px" v-for="group in selected" @click="remove(group)">{{group.name}}</button>
+<autocomplete-agent v-model="agent" placeholder="Agent..."></autocomplete-agent>
+<button class="btn btn-outline-secondary" style="margin-left: 10px" v-for="agent in selected" @click="remove(agent)">{{agent.name}}</button>
 </div>
 </template>
 
 <script>
-import AutocompleteAgentGroup from '@/Widget/Autocomplete/AgentGroup'
+import AutocompleteAgent from '@/Widget/Autocomplete/Agent'
 
 export default {
-  name: 'report-widget-agent-groups',
-  components: {AutocompleteAgentGroup},
+  name: 'report-widget-agents',
+  components: {AutocompleteAgent},
   props: ['value'],
   data () {
     return {
-      group: undefined,
+      agent: undefined,
       selected: []
     }
   },
   methods: {
-    remove (group) {
-      let index = this.selected.findIndex(el => el.id === group.id)
+    remove (agent) {
+      let index = this.selected.findIndex(el => el.id === agent.id)
       if (index >= 0) {
         this.selected.splice(index, 1)
       }
     }
   },
   watch: {
-    group (value) {
+    agent (value) {
       let index = this.selected.findIndex(el => el.id === value.id)
       if (index < 0) {
         this.selected.push(value)
