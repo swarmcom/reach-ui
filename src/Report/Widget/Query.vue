276ae376f588797a957e8b94e998635c5893c800
Roman Galeev <roman.galeev@ezuce.com>
2017-11-24 09:28:30 +0100

    Agent group autocomplete/tag widget

--- src/Report/Widget/Query.vue
+++ src/Report/Widget/Query.vue
@@ -1,4 +1,10 @@
 <template>
+<b-container>
+<b-row style="margin-bottom: 10px">
+  <b-col>
+    <widget-agent-groups v-model="agent_groups"></widget-agent-groups>
+  </b-col>
+</b-row>
 <b-row>
   <b-col cols=3>
     <widget-date v-model="date_start" placeholder="Start date"></widget-date>
@@ -6,14 +12,12 @@
   <b-col cols=3>
     <widget-date v-model="date_end" placeholder="End date"></widget-date>
   </b-col>
-  <b-col>
-    <widget-agent-groups v-model="agent_groups"></widget-agent-groups>
-  </b-col>
   <b-col cols=2>
     <button @click="apply" class="btn btn-outline-primary">Apply</button>
     <button @click="reset" class="btn btn-outline-success float-right">Reset</button>
   </b-col>
 </b-row>
+</b-container>
 </template>
 
 <script>
@@ -48,7 +52,7 @@ export default {
       return {
         date_start: this.date_start,
         date_end: this.date_end,
-        agent_groups: this.groups
+        agent_groups: this.agent_groups.map(group => group.id)
       }
     },
   }
