2e78112fa9965f2ea1e7c0d711b4d74291061903
martin harcar <martin.harcar@ezuce.com>
2017-11-27 22:12:05 +0100

    Add a filter for 'In Queue' or 'On Call' or 'All States' to the Call View Portion of queue manager

--- src/Monitor/Widget/InqueuesMonitor.vue
+++ src/Monitor/Widget/InqueuesMonitor.vue
@@ -17,6 +17,9 @@
         <b-form-select class="pointer" size="sm" v-model="selectedCustomer" style="margin-top:10px">
           <option v-for="client in this.clients" :value=client.name>{{client.name}}</option>
         </b-form-select>
+        <b-form-select class="pointer" size="sm" v-model="selectedState" style="margin-top:10px">
+          <option v-for="state in this.states" :value=state.name>{{state.name}}</option>
+        </b-form-select>
       </div>
       <div class="col-10">
         <b-table style="margin-top:10px" small bordered
@@ -80,9 +83,15 @@ export default {
       clients: [],
       queues: [],
       lines: [],
+      states: [
+        {name: "Any State"},
+        {name: "oncall"},
+        {name: "inqueue"}
+      ],
       selectedQueue: 'Any Queue',
       selectedCustomer: 'Any Customers',
       selectedLine: 'Any Lines',
+      selectedState: 'Any State',
       filter: null,
       sortBy: 'agent_id',
       sortDesc: false,
@@ -149,6 +158,9 @@ export default {
             return
         }
         else if(this.selectedCustomer != 'Any Customers')
+          return
+
+        if(this.selectedState != key.state && this.selectedState != 'Any State')
           return
 
         compInqueues.push(key);
