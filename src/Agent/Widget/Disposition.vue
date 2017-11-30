4cfff3a747465bbc5884e8aa86370201a696bfd9
Roman Galeev <roman.galeev@ezuce.com>
2017-11-30 12:11:37 +0100

    Disable dispositions widget if there are none

--- src/Agent/Widget/Disposition.vue
+++ src/Agent/Widget/Disposition.vue
@@ -1,5 +1,5 @@
 <template>
-<b-dropdown text="Disposition" variant="outline-primary">
+<b-dropdown text="Disposition" :disabled="isDisabled()" variant="outline-primary">
   <b-dropdown-item v-for="r in dispositions" :key="r.id" @click="disposition(r.id)">{{ r.name }}</b-dropdown-item>
 </b-dropdown>
 </template>
@@ -14,6 +14,9 @@ export default {
     }
   },
   methods: {
+    isDisabled() {
+      return this.dispositions.length == 0
+    },
     query: async function () {
       this.dispositions = await this.$agent.p_mfa('ws_agent', 'dispositions', [this.uuid])
     },
@@ -21,7 +24,7 @@ export default {
       this.$agent.p_mfa('ws_agent', 'disposition', [this.uuid, id])
     },
   },
-  mounted () {
+  created () {
     this.query()
   }
 }
