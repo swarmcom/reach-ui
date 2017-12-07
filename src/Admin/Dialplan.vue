cb2176d8e1e4e9a5f78654c42cb68f60760f4ccb
Roman Galeev <roman.galeev@ezuce.com>
2017-12-08 00:29:11 +0100

    Allow to change dialplan entries order

--- src/Admin/Dialplan.vue
+++ src/Admin/Dialplan.vue
@@ -4,12 +4,19 @@
     <b-col><h3>Dialplan:</h3></b-col>
   </b-row>
 
-  <b-form-row v-for="dial of dials" :key="dial.id" style="margin-top: 5px">
-    <b-col cols=1>
+  <b-form-row v-for="dial of entries" :key="dial.id" style="margin-top: 5px">
+    <b-col cols=2>
       <button @click="del(dial.id)" class="btn btn-outline-danger">
         <icon class="align-middle" name="minus" scale="1"></icon>
       </button>
+      <button @click="up(dial.id)" class="btn btn-outline-success">
+        <icon class="align-middle" name="arrow-up" scale="1"></icon>
+      </button>
+      <button @click="down(dial.id)" class="btn btn-outline-primary">
+        <icon class="align-middle" name="arrow-down" scale="1"></icon>
+      </button>
     </b-col>
+
     <b-col cols=3>
       <select class="custom-select" v-model="dial.header" v-on:change="onChange(dial)">
         <option></option>
@@ -25,7 +32,7 @@
   </b-form-row>
 
   <b-form-row style="margin-top: 5px">
-    <b-col cols=1>
+    <b-col cols=2>
       <button @click="add" class="btn btn-outline-secondary">
         <icon name="plus" scale="1" class="align-middle"></icon>
       </button>
@@ -50,7 +57,7 @@
 import LineIns from '@/Widget/LineIns'
 
 export default {
-  name: 'admin-dials',
+  name: 'admin-dialplan',
   components: {
     lines: LineIns
   },
@@ -65,28 +72,34 @@ export default {
       match: undefined,
       line_id: undefined,
       header: undefined,
-      dials: []
+      entries: []
     }
   },
   methods: {
     query: async function () {
-      this.dials = await this.$agent.p_mfa('ws_db_dial', 'get')
+      this.entries = await this.$agent.p_mfa('ws_db_dial', 'get')
     },
     onChange (dial) {
       this.$agent.p_mfa('ws_db_dial', 'update', [dial.id, dial])
     },
+    up: async function (id) {
+      this.entries = await this.$agent.p_mfa('ws_db_dial', 'up', [id])
+    },
+    down: async function (id) {
+      this.entries = await this.$agent.p_mfa('ws_db_dial', 'down', [id])
+    },
     add: async function() {
       let dial = await this.$agent.p_mfa('ws_db_dial', 'create', [{ match: this.match, line_in_id: this.line_id, header: this.header }])
-      this.dials.push(dial)
+      this.entries.push(dial)
       this.match = ''
       this.line_id = ''
       this.header = ''
     },
     del: async function (Key) {
       await this.$agent.p_mfa('ws_db_dial', 'delete', [Key])
-      let id = this.dials.findIndex(Obj => Obj.id === Key)
+      let id = this.entries.findIndex(Obj => Obj.id === Key)
       if (id >= 0) {
-        this.dials.splice(id, 1)
+        this.entries.splice(id, 1)
       }
     }
   },
