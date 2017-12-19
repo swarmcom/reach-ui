3073e84cacf63ebc578af29372664e99d1126555
martin harcar <martin.harcar@ezuce.com>
2017-12-19 10:47:25 +0100

    Make conference to agent similar to old UI

--- src/Agent/Widget/TransferConference.vue
+++ src/Agent/Widget/TransferConference.vue
@@ -3,24 +3,24 @@
   <toggle-bar></toggle-bar>
   <b-collapse v-model="showCollapse" id="collapseTransferConference" class="mt-2">
     <b-row>
-      <b-col cols="3">
+      <b-col cols="4">
         <b-form-select v-model="selected">
         <option :value="null">Transfer / Conference</option>
         <option :value="'queue'">Queue...</option>
         <option :value="'agent'">Agent...</option>
         <option v-if="this.$agent.can_call()" :value="'number'">Number...</option>
         </b-form-select>
-        <b-form-input class="customInput" v-if="selected=='agent'" v-model="filter" placeholder="Search..." />
+        <b-form-input class="customInput" v-if="selected==='agent'" v-model="filter" placeholder="Search..." />
       </b-col>
-      <b-col cols="3" v-if="(selected=='queue' || selected=='number')">
-        <b-form-select v-if="selected=='queue'" v-model="selectedQueue">
+      <b-col cols="4" v-if="(selected==='queue' || selected==='number')">
+        <b-form-select v-if="selected==='queue'" v-model="selectedQueue">
           <option :value="null">Select Queue...</option>
           <option v-for="queue in queues" :key="queue.id" :value="queue.id">{{queue.name}}</option>
         </b-form-select>
-        <b-form-input class="customInput" v-if="selected=='number'" v-model="selectedNumber" type="text">
+        <b-form-input class="customInput" v-if="selected==='number'" v-model="selectedNumber" type="text">
         </b-form-input>
       </b-col>
-      <b-col cols="9" v-if="selected=='agent'">
+      <b-col cols="8" v-if="selected==='agent'">
         <b-table small bordered
           tbody-tr-class="pointer"
           @row-clicked="onSelectAgent"
@@ -28,12 +28,11 @@
           :fields="fieldsAgents"
           :filter="filter">
         </b-table>
-        <div>Selected Agent: <strong>{{ selectedAgent }}</strong></div>
       </b-col>
     </b-row>
     <b-row>
     <b-col cols="9" order="3" style="margin-top:5px" v-if="selected=='queue' && selectedQueue">
-      <queue-skills v-if="uuid!=undefined" :uuid="uuid""></queue-skills>
+      <queue-skills v-if="uuid!==undefined" :uuid="uuid"></queue-skills>
     </b-col>
     </b-row>
     <b-row style="margin-top:10px">
@@ -64,9 +63,9 @@ export default {
       showCollapse: true,
       selected: null,
       fieldsAgents: {
-        id: { label: 'Id', thClass:"table-header-text-center", tdClass:"table-body-text-center" },
-        state: { label: 'State', sortable: true, thClass:"table-header-text-center", tdClass:"table-body-text-center" },
-        name: { label: 'Name', thClass:"table-header-text-center", tdClass:"table-body-text-center" }
+        name: { label: 'Name', sortable: true, thClass:"table-header-text-center", tdClass:"table-body-text-center" },
+        group: { label: 'Group', sortable: true, thClass:"table-header-text-center", tdClass:"table-body-text-center", formatter: (value) => { return value.name } },
+        state: { label: 'State', sortable: true, thClass:"table-header-text-center", tdClass:"table-body-text-center" }
       },
       selectedQueue: 'null',
       selectedAgent: 'null',
@@ -80,29 +79,29 @@ export default {
       this.queues = await this.$agent.p_mfa('ws_agent', 'get_transfer_queues')
     },
     conference () {
-      if (this.selected === 'queue' && this.selectedQueue != 'null') {
+      if (this.selected === 'queue' && this.selectedQueue !== 'null') {
         this.$agent.conference_to_queue(this.selectedQueue)
       }
-      else if (this.selected === 'agent' && this.selectedAgent != 'null') {
+      else if (this.selected === 'agent' && this.selectedAgent !== 'null') {
         this.$agent.conference_to_agent(this.selectedAgent)
       }
-      else if (this.selected === 'number' && this.selectedNumber != 'null') {
+      else if (this.selected === 'number' && this.selectedNumber !== 'null') {
         this.$agent.conference_to_uri(this.selectedNumber)
       }
     },
     transfer () {
-      if (this.selected === 'queue' && this.selectedQueue != 'null') {
+      if (this.selected === 'queue' && this.selectedQueue !== 'null') {
         this.$agent.transfer_to_queue(this.selectedQueue)
       }
-      else if (this.selected === 'agent' && this.selectedAgent != 'null') {
+      else if (this.selected === 'agent' && this.selectedAgent !== 'null') {
         this.$agent.transfer_to_agent(this.selectedAgent)
       }
-      else if (this.selected === 'number' && this.selectedNumber != 'null') {
+      else if (this.selected === 'number' && this.selectedNumber !== 'null') {
         this.$agent.transfer_to_uri(this.selectedNumber)
       }
     },
     onSelectAgent (data) {
-      this.selectedAgent = data.id
+      this.selectedAgent === data.id ? this.selectedAgent = 'null' : this.selectedAgent = data.id
     },
   },
   computed: {
@@ -110,14 +109,23 @@ export default {
       let agents = this.$agent.vm.transfer_agents.slice(0)
       agents.forEach( (key) => {
         key._rowVariant = 'primary'
+        if (key.id === this.selectedAgent)
+          key._rowVariant = 'warning'
       })
       return agents
+    }
+  },
+  watch: {
+    selected: function(val) {
+      this.selectedAgent = 'null'
+      this.selectedQueue = 'null'
+      this.selectedNumber = 'null'
     }
   },
   created () {
     this.a = this.$agent.getData()
     this.query()
-    if (this.a.storage_data.smtransferConferenceCollapsed != undefined)
+    if (this.a.storage_data.smtransferConferenceCollapsed !== undefined)
       this.showCollapse = this.a.storage_data.smtransferConferenceCollapsed
   }
 }
