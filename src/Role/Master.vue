8c8795b151e0ece794cbf1e1ef6a93f11da4b05d
martin harcar <martin.harcar@ezuce.com>
2018-01-11 10:40:07 +0100

    Use b-nav in master

--- src/Role/Master.vue
+++ src/Role/Master.vue
@@ -1,46 +1,43 @@
 <template>
-<div style="min-height: 100%; padding-top: 70px; padding-bottom: 60px">
-<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  <router-link class="navbar-brand" to="/">Reach UI</router-link>
-  <div class="collapse navbar-collapse" id="navbars">
-    <ul class="navbar-nav mr-auto">
-      <b-nav-item-dropdown text="Instance">
+  <div style="min-height: 100%; padding-bottom: 60px">
+    <b-navbar class="navbar-custom fixed-top" toggleable="md" type="dark" variant="info">
+      <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+      <b-navbar-brand to="/">HOME</b-navbar-brand>
+      <b-collapse is-nav id="nav_collapse">
+        <b-navbar-nav>
+          <b-nav-item @click="logout">LOGOUT</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-nav class="custom-b-nav" tabs>
+      <b-nav-item-dropdown text="INSTANCE">
         <b-dropdown-item to="/kam/nodes">Nodes</b-dropdown-item>
         <b-dropdown-item to="/kam/domains">Domains</b-dropdown-item>
         <b-dropdown-item to="/kam/registry">Registry</b-dropdown-item>
       </b-nav-item-dropdown>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/params">Params</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link class="nav-link" to="/help">Help</router-link>
-      </li>
-    </ul>
-    <span class="navbar-text" style="margin-right: 20px">{{ this.$agent.vm.agent.login }}</span>
-    <button @click="logout" class="btn btn-sm btn-outline-danger">Logout</button>
-  </div>
-</nav>
-<div class="container">
-  <transition name="reach" mode="out-in">
-    <router-view></router-view>
-  </transition>
-</div>
-
-<footer class="footer">
-  <div class="container">
-    <div class="row justify-content-center">
-      <small>
-        Master &copy; 2018 eZuce
-        UI: <a target="_blank" :href="ref_ui_uri()">{{ ref_ui }}</a>
-        Backend: <a target="_blank" :href="ref_backend_uri()">{{ ref_backend }}</a>
-      </small>
+      <b-nav-item to="/params">PARAMS</b-nav-item>
+    </b-nav><!--container-fluid-->
+    <div class="container-fluid">
+      <transition name="reach" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </div>
+
+    <footer class="footer">
+      <div class="container">
+        <div class="row justify-content-center">
+          <small>
+            Agent &copy; 2018 eZuce
+            UI: <a target="_blank" :href="ref_ui_uri()">{{ ref_ui }}</a>
+            Backend: <a target="_blank" :href="ref_backend_uri()">{{ ref_backend }}</a>
+          </small>
+        </div>
+      </div>
+    </footer>
+
+    <notifications position="bottom right" :speed="500" :duration="1000"/>
+
   </div>
-</footer>
-
-<notifications position="bottom right" :speed="500" :duration="1000"/>
-
-</div>
 </template>
 
 <script>
@@ -69,6 +66,6 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../../custom-bootstrap.scss";
+//@import "../../custom-bootstrap.scss";
 @import "../../node_modules/bootstrap/scss/bootstrap.scss";
 </style>
