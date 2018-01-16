b9044244c7e94e07f88366b345b00d6132c2fd96
martin harcar <martin.harcar@ezuce.com>
2018-01-16 15:16:06 +0100

    Add bootstrap.scss

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import Master from '@/Role/Master'
 import Login from '@/Login'
 
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/scss/bootstrap.scss'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 import 'vue-awesome/icons'
