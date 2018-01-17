1fbab8c13b159078529ef9635c181d27c9e59b2e
Roman Galeev <roman.galeev@ezuce.com>
2018-01-17 17:56:54 +0100

    Allow to select verbose values of call recordings parameter

---
+++ src/Widget/FormCallRecording.vue
@@ -0,0 +1,58 @@
+<template>
+<div class="row" style="margin-top: 5px">
+  <label :id="label" class="col-3 col-form-label">{{ label }}</label>
+  <div v-if="effective" class="col-5">
+    <b-form-select :value="computed_value" :options="options" v-on:change="onChange"></b-form-select>
+  </div>
+  <div v-if="effective" class="col-4">
+    <b-form-select :value="computed_effective" :options="options" disabled></b-form-select>
+  </div>
+  <div v-else class="col-9">
+    <b-form-select :value="computed_value" :options="options" v-on:change="onChange"></b-form-select>
+  </div>
+</div>
+</template>
+
+<script>
+function verbose(value) {
+  console.log("value", value)
+  if (value === true) {
+    return 'Enabled'
+  } else if (value === false) {
+    return 'Disabled'
+  } else {
+    return 'On Demand'
+  }
+}
+
+export default {
+  name: 'form-bool-drop',
+  props: ['label', 'value', 'effective'],
+  data () {
+    return {
+      options: ['On Demand', 'Enabled', 'Disabled']
+    }
+  },
+  computed: {
+    computed_value () {
+      return verbose(this.value)
+    },
+    computed_effective () {
+      return verbose(this.effective)
+    }
+  },
+  methods: {
+    onChange (V) {
+      if (V == 'On Demand') {
+        this.$emit('input', null)
+      } else if (V == 'Enabled') {
+        this.$emit('input', true)
+      } else if (V == 'Disabled') {
+        this.$emit('input', false)
+      }
+    }
+  },
+  created () {
+  }
+}
+</script>
