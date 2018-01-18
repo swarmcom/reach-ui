48d717aab4ead3cd6bfdcc05068531f0136aeb9a
martin harcar <martin.harcar@ezuce.com>
2018-01-18 09:27:55 +0100

    Allow more than 1 Line Out to be associated with an agent group and/or agent

--- src/Widget/Lines.vue
+++ src/Widget/Lines.vue
@@ -1,19 +1,25 @@
 <template>
-<div class="row" style="margin-top: 5px">
+<b-row style="margin-top: 5px">
   <label :id="label" class="col-3 col-form-label">{{ label }}</label>
-  <div class="col-9">
+  <b-col cols="2">
     <select class="custom-select pointer" style="width: 100%" :value="value" :disabled="isDisabled()" @change="onUpdate($event.target.value)">
       <option></option>
-      <option v-for="line in lines" :value="line.id" :selected="isActive(line.id)">{{ line.name }}</option>
+      <option v-for="line in lines" :value="line.id">{{ line.name }}</option>
     </select>
-  </div>
-</div>
+  </b-col>
+  <b-col cols="3" style="margin-top: 5px">
+    <button class="btn btn-sm btn-outline-primary" style="margin-left: 10px" v-for="line in value" @click="remove(line.id)">{{line.name}}</button>
+  </b-col>
+  <b-col v-if="effective" cols="4">
+    <button class="btn btn-sm btn-outline-primary" style="margin-left: 10px" v-for="line in effective" disabled>{{line.name}}</button>
+  </b-col>
+</b-row>
 </template>
 
 <script>
 export default {
   name: 'form-text',
-  props: ['label', 'value'],
+  props: ['label', 'value', 'effective'],
   data () {
     return {
       lines: []
@@ -29,8 +35,27 @@ export default {
     query: async function () {
       this.lines = await this.$agent.p_mfa('ws_db_line_out', 'get')
     },
+    remove (value) {
+      let index = this.value.findIndex(el => el.id == value)
+      if (index >= 0) {
+        this.value.splice(index, 1)
+      }
+    },
     onUpdate (value) {
-      this.$emit('input', value)
+      if (value == undefined || value == "" || this.value == undefined) {
+        return
+      }
+      let index = this.value.findIndex(el => el.id == value)
+      if (index < 0) {
+        let index = this.lines.findIndex(el => el.id == value)
+          if(index >= 0)
+            this.value.push({id:this.lines[index].id, name:this.lines[index].name})
+      }
+      else {
+          this.value.splice(index, 1)
+      }
+
+      this.$emit('input', this.value)
     }
   },
   created () {
