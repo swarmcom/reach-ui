ddbcc85959c2be9aa0183c1de7d57ea136a23368
Roman Galeev <roman.galeev@ezuce.com>
2018-01-22 12:14:48 +0100

    Reset URI on agent's logout

--- src/agent.js
+++ src/agent.js
@@ -88,6 +88,7 @@ export default class Agent extends WsProto {
 
   // AGENT API
   logout () {
+    window.location.href = '#'
     localStorage.removeItem('session-key')
     this.call('stop', [], () => this.handleAuth())
     this.connect()
