31ed3af07c908f90d1cb7e0f01db63d63cc635c5
Roman Galeev <roman.galeev@ezuce.com>
2018-01-25 00:55:06 +0100

    Allow agent to place call only if there are lines out in list

--- src/agent.js
+++ src/agent.js
@@ -227,7 +227,7 @@ export default class Agent extends WsProto {
   is_barge () { return this.vm && this.vm.state == 'barge' }
   is_conference () { return this.vm && ( this.vm.state == 'conference' || this.vm.state == 'inconference' ) }
   can_login () { return this.vm.session_auth }
-  can_call () { return this.vm && (this.vm.agent.line_id || this.vm.agent.lines) }
+  can_call () { return this.vm && this.vm.agent.lines && this.vm.agent.lines.length > 0 }
   can_hangup () { return this.vm && ( this.vm.state == 'hold' || this.vm.state == 'oncall' || this.vm.state == 'outgoing' || this.vm.state == 'ringing' || this.vm.state == 'conference' || this.vm.state == 'inconference' || this.vm.state == 'test') }
   can_conference () { return this.vm && ( this.vm.state == 'oncall' || this.vm.state == 'conference' || this.vm.state == 'inconference') }
   can_transfer () { return this.vm && ( this.vm.state == 'oncall' || this.vm.state == 'conference' || this.vm.state == 'inconference') }
