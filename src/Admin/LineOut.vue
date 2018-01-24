56fafea540f7d955bb88c2827de492a3cba563fa
Roman Galeev <roman.galeev@ezuce.com>
2018-01-24 01:20:53 +0100

    Add call recording flag to line out entity

--- src/Admin/LineOut.vue
+++ src/Admin/LineOut.vue
@@ -3,6 +3,7 @@
   <form-text label="Line-Out Name" v-model="rec.name"></form-text>
   <form-text label="Line-Out Description" v-model="rec.description"></form-text>
   <clients label="Client" v-model="rec.client_id"></clients>
+  <form-call-recording label="Call recording" v-model="rec.enable_call_recording"></form-call-recording>
   <form-select-bool label="Override Caller ID (outbound)" v-model="rec.override_clid"></form-select-bool>
   <form-text label="Caller ID name" v-model="rec.caller_id_name"></form-text>
   <form-text label="Caller ID number" v-model="rec.caller_id_number"></form-text>
@@ -18,11 +19,13 @@
 <script>
 import Common from '@/Admin/Common'
 import Obj from '@/Admin/Object'
+import FormCallRecording from '@/Widget/FormCallRecording'
 
 export default {
   name: 'admin-line-out',
   props: ['id'],
   mixins: [Common, Obj],
+  components: { 'form-call-recording': FormCallRecording },
   data () {
     return {
       rec: {},
