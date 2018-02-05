28157daba73dbb46f3f0c509d6fe161259586437
Pavel Farkas <pavel.farkas@ezuce.com>
2018-02-05 20:35:00 +0100

    Report template component + new/modified input controls components
    Report template component has 2 named slots:
    - for input controls
    - for actual report content
    
    Old input control components deleted

---
+++ src/Report/Input/Interval.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>Interval [min]:</div>
+    <div style="padding-bottom: 30px">
+      <b-input :value="interval" @input="checkValue" min="1" step="1" size="sm" type="number"></b-input>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'interval-input',
+    props: {
+      value: {
+        type: Number,
+        required: true
+      }
+    },
+    data () {
+      return {
+        interval: this.value
+      }
+    },
+    methods: {
+      checkValue(v) {
+        this.interval = Number(v)
+        this.$emit('input', this.interval)
+      }
+    },
+    watch: {
+      value (v) {
+        this.interval = v
+        return v
+      }
+    }
+  }
+</script>
