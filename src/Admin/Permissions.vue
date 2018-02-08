0e11685ffd1ce4ccececd371db8a314e9a452070
Roman Galeev <roman.galeev@ezuce.com>
2018-02-08 13:24:02 +0100

    Rework permissions

--- src/Admin/Permissions.vue
+++ src/Admin/Permissions.vue
@@ -1,182 +1,160 @@
 <template>
 <div class="container">
-  <b-row style="margin-bottom: 10px"><h3>Permissions:</h3></b-row>
-  <div v-if="ui == 'supervisor'" v-for="(perm, index) of perms_check" style="margin-top: 5px">
-    <b-row v-if="index === 0"><b>Content</b></b-row>
-    <b-row v-if="index === 0"> <b-col>Displayed Tabs: </b-col></b-row>
-    <b-row v-if="index === 4"> <b>Accessible Widgets: </b></b-row>
-    <b-row v-if="index === 7"><b>Agent Groups:</b></b-row>
-    <b-row v-if="index === (7+groups.length)"><b>Queues:</b></b-row>
-    <b-row v-if="index === (7+groups.length + queues.length)"><b>Supervisor Privileges:</b></b-row>
-    <b-row v-if="index === (14+groups.length + queues.length)"> <b>Features: </b></b-row>
-    <b-row v-if="index === (17+groups.length + queues.length)"><b-col> Conference/Transfer Privileges</b-col></b-row>
-    <b-row v-if="index === (24+groups.length + queues.length)"><b> Call Recording</b></b-row>
-    <b-row v-if="index === (24+groups.length + queues.length)"><b-col> Line In</b-col></b-row>
-    <b-row v-if="index === (24+groups.length + queues.length + line_ins.length)"><b-col> Line Out</b-col></b-row>
-    <b-form-checkbox v-if="canShow(index)" v-model="perm.value" v-on:change="onChangeCheck(perm, $event)">
-      {{perm.displayName}}
-    </b-form-checkbox>
-  </div>
-  <div v-if="ui == 'agent'" v-for="(perm, index) of perms_check" style="margin-top: 5px">
-    <b-row v-if="index === 0"><b>Content</b></b-row>
-    <b-row v-if="index === 0"> <b-col>Displayed Tabs: </b-col></b-row>
-    <b-row v-if="index === 1"> <b>Accessible Widgets: </b></b-row>
-    <b-row v-if="index === 4"><b>Agent Groups:</b></b-row>
-    <b-row v-if="index === (4+groups.length)"><b>Queues:</b></b-row>
-    <b-row v-if="index === (4 + groups.length + queues.length)"> <b>Features: </b></b-row>
-    <b-row v-if="index === (7 + groups.length + queues.length)"><b-col> Conference/Transfer Privileges</b-col></b-row>
-    <b-row v-if="index === (14 + groups.length + queues.length)"><b> Administrative Control:</b></b-row>
-    <b-form-checkbox v-model="perm.value" v-on:change="onChangeCheck(perm, $event)">
-      {{perm.displayName}}
-    </b-form-checkbox>
-  </div>
-  <button @click="selectAll" class="btn btn-primary pointer">Select All</button>
+  <b-row style="margin-bottom: 10px"><b-col><h3>Permissions:</h3></b-col></b-row>
+  <b-row>
+    <b-col>
+      <b-row> <b-col><h4>Available Tabs:</h4></b-col></b-row>
+      <b-row v-for="p of this.ui" :key="p">
+          <b-col cols="10">{{names[p].name}}</b-col>
+          <b-col cols="2"><b-form-checkbox v-model="effective[p]" v-on:change="onChange(p, $event)"></b-form-checkbox></b-col>
+      </b-row>
+    </b-col>
+    <b-col>
+      <b-row> <b-col><h4>Available Widgets:</h4></b-col></b-row>
+      <b-row v-for="p of this.widgets" :key="p">
+          <b-col cols="10">{{names[p].name}}</b-col>
+          <b-col cols="2"><b-form-checkbox v-model="effective[p]" v-on:change="onChange(p, $event)"></b-form-checkbox></b-col>
+      </b-row>
+    </b-col>
+  </b-row>
+
+  <b-row style="margin-top: 20px">
+    <b-col>
+      <b-row><b-col><h4>UI Features:</h4></b-col></b-row>
+      <b-row v-for="p of this.features" :key="p">
+          <b-col cols="10">{{names[p].name}}</b-col>
+          <b-col cols="2"><b-form-checkbox v-model="effective[p]" v-on:change="onChange(p, $event)"></b-form-checkbox></b-col>
+      </b-row>
+    </b-col>
+    <b-col>
+      <b-row><b-col><h4>Call Features:</h4></b-col></b-row>
+      <b-row v-for="p of this.call_features" :key="p">
+          <b-col cols="10">{{names[p].name}}</b-col>
+          <b-col cols="2"><b-form-checkbox v-model="effective[p]" v-on:change="onChange(p, $event)"></b-form-checkbox></b-col>
+      </b-row>
+      <b-row><b-col><h4>Agent Features:</h4></b-col></b-row>
+      <b-row v-for="p of this.agent_features" :key="p">
+          <b-col cols="10">{{names[p].name}}</b-col>
+          <b-col cols="2"><b-form-checkbox v-model="effective[p]" v-on:change="onChange(p, $event)"></b-form-checkbox></b-col>
+      </b-row>
+
+    </b-col>
+  </b-row>
+
 </div>
 </template>
 
 <script>
 export default {
   name: 'admin-role-permissions',
-  props: ['id','ui'],
+  props: ['id'],
   data () {
     return {
       name: undefined,
-      permissions: [],
-      perms_check: []
+      effective: {},
+      ui: [
+        'main-ui',
+        'admin-ui',
+        'profile-ui',
+        'recordings-ui',
+        'reports-ui',
+        'monitor-ui'
+      ],
+      widgets: [
+        'myStatistics-widget',
+        'queueManager-widget',
+        'agentManager-widget'
+      ],
+      features: [
+        'controlAgentState-feature',
+        'myPhone-feature',
+        'outbound-feature',
+        'CROnDemand-feature',
+        'transAgent-feature',
+        'transQueue-feature',
+        'transNumber-feature',
+        'confAgent-feature',
+        'confQueue-feature',
+        'confNumber-feature',
+        'transConfChangeSkills-feature'
+      ],
+      call_features: [
+        'sip-can-register',
+        'monitor-feature',
+        'whisper-feature',
+        'barge-feature',
+        'takeOver-feature',
+        'takeCallQueue-feature',
+        'hangupCallQueue-feature'
+      ],
+      agent_features: [
+        "agentName-edit",
+        "agentPassword-edit",
+        "agentLineOut-visible",
+        "primarySip-edit",
+        "primarySip-edit",
+        "ringTimeout-edit",
+        "maxMissedCalls-edit",
+        "maxRingsSuccess-edit",
+        "autoLogout-visible",
+        "avatar-visible",
+        "agentSkills-visible"
+      ],
+      names: {
+        "main-ui": { name: "Main Tab" },
+        "admin-ui": { name: "Admin" },
+        "profile-ui": { name: "Profile" },
+        "recordings-ui": { name: "Recordings" },
+        "reports-ui": { name: "Reports" },
+        "monitor-ui": { name: "Monitor" },
+        "agentManager-widget": { name: "Agent Manger" },
+        "queueManager-widget": { name: "Queue Manager" },
+        "myStatistics-widget": { name: "My Statistics" },
+        "controlAgentState-feature": { name: "Control Agent State" },
+        "monitor-feature": { name: "Monitor" },
+        "barge-feature": { name: "Barge" },
+        "whisper-feature": { name: "Whisper" },
+        "takeOver-feature": { name: "Take Over" },
+        "takeCallQueue-feature": { name: "Take call from queue" },
+        "hangupCallQueue-feature": { name: "Hangup call from queue"},
+        "myPhone-feature": { name: "Set My Phone" },
+        "outbound-feature": { name: "Allow Outbound" },
+        "CROnDemand-feature": { name: "Allow On Demand Call Recording" },
+        "transAgent-feature": { name: "Transfer to Agent" },
+        "transQueue-feature": { name: "Transfer to Queue" },
+        "transNumber-feature": { name: "Transfer to Number" },
+        "confAgent-feature": { name: "Conference to Agent" },
+        "confQueue-feature": { name: "Conference to Queue" },
+        "confNumber-feature": { name: "Conference to Number" },
+        "transConfChangeSkills-feature": { name: "Change Skills on Conf/Tran" },
+        "sip-can-register": { name: "Allow to register SIP phone" },
+        "agentName-edit": { name: "Name" },
+        "agentPassword-edit": { name: "Password" },
+        "agentLineOut-visible": { name: "Line Out" },
+        "primarySip-edit": { name: "Primary SIP Contact" },
+        "primarySip-edit": { name: "SIP Contacts" },
+        "ringTimeout-edit": { name "Ring Timeout" },
+        "maxMissedCalls-edit": { name: "Max Missed Calls" },
+        "maxRingsSuccess-edit": { name: "Reset Max Rings On Success" },
+        "autoLogout-visible": { name: "Auto Logout" },
+        "avatar-visible": { name: "Avatar" },
+        "agentSkills-visible": { name: "Agent Skills" }
+      }
     }
   },
   methods: {
     query: async function () {
-      this.groups = await this.$agent.p_mfa('ws_db_agent_group', 'get')
-      this.queues = await this.$agent.p_mfa('ws_db_queue', 'get', [])
-      this.line_ins = await this.$agent.p_mfa('ws_db_line_in', 'get')
-      this.line_outs = await this.$agent.p_mfa('ws_db_line_out', 'get')
-      this.perms_check = []
-      if (this.ui == 'supervisor') {
-        this.perms_check.push({displayName: "Main", name: "main-ui", value: false})
-        this.perms_check.push({displayName: "Monitor", name: "monitor-ui", value: false})
-        this.perms_check.push({displayName: "Recordings", name: "recordings-ui", value: false})
-        this.perms_check.push({displayName: "Reports", name: "reports-ui", value: false})
-        this.perms_check.push({displayName: "Agent Manager", name: "agentManager-widget", value: false})
-        this.perms_check.push({displayName: "Queue Manager", name: "queueManager-widget", value: false})
-        this.perms_check.push({displayName: "My Statistics", name: "myStatistics-widget", value: false})
-        this.groups.forEach( (key) => {
-          this.perms_check.push({displayName: key.name, name: key.name+'-groups', value: false})
-        })
-        this.queues.forEach( (key) => {
-            this.perms_check.push({displayName: key.name, name: key.name+'-queues', value: false})
-        })
-        this.perms_check.push({displayName: "Control Agent State", name: "controlAgentState-feature", value: false})
-        this.perms_check.push({displayName: "Monitor", name: "monitor-feature", value: false})
-        this.perms_check.push({displayName: "Barge", name: "barge-feature", value: false})
-        this.perms_check.push({displayName: "Whisper", name: "whisper-feature", value: false})
-        this.perms_check.push({displayName: "Take Over", name: "takeOver-feature", value: false})
-        this.perms_check.push({displayName: "Take call from queue", name: "takeCallQueue-feature", value: false})
-        this.perms_check.push({displayName: "Hangup call from queue", name: "hangupCallQueue-feature", value: false})
-        this.perms_check.push({displayName: "Set My Phone", name: "myPhone-feature", value: false})
-        this.perms_check.push({displayName: "Allow Outbound", name: "outbound-feature", value: false})
-        this.perms_check.push({displayName: "Allow On Demand Call Recording", name: "CROnDemand-feature", value: false})
-        this.perms_check.push({displayName: "Transfer to Agent", name: "transAgent-feature", value: false})
-        this.perms_check.push({displayName: "Transfer to Queue", name: "transQueue-feature", value: false})
-        this.perms_check.push({displayName: "Transfer to Number", name: "transNumber-feature", value: false})
-        this.perms_check.push({displayName: "Conference to Agent", name: "confAgent-feature", value: false})
-        this.perms_check.push({displayName: "Conference to Queue", name: "confQueue-feature", value: false})
-        this.perms_check.push({displayName: "Conference to Number", name: "confNumber-feature", value: false})
-        this.perms_check.push({displayName: "Change Skills on Conf/Tran", name: "transConfChangeSkills-feature", value: false})
-        this.line_ins.forEach( (key) => {
-          this.perms_check.push({displayName: key.name, name: key.name+'-line_ins', value: false})
-        })
-        this.line_outs.forEach( (key) => {
-            this.perms_check.push({displayName: key.name, name: key.name+'-line_outs', value: false})
-        })
-      }
-      else if (this.ui == 'agent') {
-        this.perms_check.push({displayName: "Profile", name: "profile-ui", value: false})
-        this.perms_check.push({displayName: "Agent Manager", name: "agentManager-widget", value: false})
-        this.perms_check.push({displayName: "Queue Manager", name: "queueManager-widget", value: false})
-        this.perms_check.push({displayName: "My Statistics", name: "myStatistics-widget", value: false})
-        this.groups.forEach( (key) => {
-          this.perms_check.push({displayName: key.name, name: key.name+'-groups', value: false})
-        })
-        this.queues.forEach( (key) => {
-          this.perms_check.push({displayName: key.name, name: key.name+'-queues', value: false})
-        })
-        this.perms_check.push({displayName: "Set My Phone", name: "myPhone-feature", value: false})
-        this.perms_check.push({displayName: "Allow Outbound", name: "outbound-feature", value: false})
-        this.perms_check.push({displayName: "Allow On Demand Call Recording", name: "CROnDemand-feature", value: false})
-        this.perms_check.push({displayName: "Transfer to Agent", name: "transAgent-feature", value: false})
-        this.perms_check.push({displayName: "Transfer to Queue", name: "transQueue-feature", value: false})
-        this.perms_check.push({displayName: "Transfer to Number", name: "transNumber-feature", value: false})
-        this.perms_check.push({displayName: "Conference to Agent", name: "confAgent-feature", value: false})
-        this.perms_check.push({displayName: "Conference to Queue", name: "confQueue-feature", value: false})
-        this.perms_check.push({displayName: "Conference to Number", name: "confNumber-feature", value: false})
-        this.perms_check.push({displayName: "Change Skills on Conf/Tran", name: "transConfChangeSkills-feature", value: false})
-        this.perms_check.push({displayName: "Agent Name", name: "agentName-edit", value: false})
-        this.perms_check.push({displayName: "Password", name: "agentPassword-edit", value: false})
-        this.perms_check.push({displayName: "Line Out", name: "agentLineOut-visible", value: false})
-        this.perms_check.push({displayName: "Primary SIP Contact", name: "primarySip-edit", value: false})
-        this.perms_check.push({displayName: "SIP Contacts", name: "additionalSip-visible", value: false})
-        this.perms_check.push({displayName: "Ring Timeout", name: "ringTimeout-edit", value: false})
-        this.perms_check.push({displayName: "Max Missed Calls(auto release)", name: "maxMissedCalls-edit", value: false})
-        this.perms_check.push({displayName: "Reset Max Rings On Success", name: "maxRingsSuccess-edit", value: false})
-        this.perms_check.push({displayName: "Auto Logout", name: "autoLogout-visible", value: false})
-        this.perms_check.push({displayName: "Avatar", name: "avatar-visible", value: false})
-        this.perms_check.push({displayName: "Agent Skills", name: "agentSkills-visible", value: false})
-      }
-      this.permissions = await this.$agent.p_mfa('ws_db_permission', 'get', [this.id])
-        this.permissions.forEach( (key) =>
-          {
-            let i = this.perms_check.findIndex(E => E.name === key.name)
-            if (i >= 0) {
-              this.perms_check[i].value = true
-              this.perms_check[i].id = key.id
-            }
-          }
-        )
+      this.effective = await this.$agent.p_mfa('ws_db_permission', 'map', [this.id])
     },
-    onChangeCheck: async function(perm, checked) {
-      if(checked) {
-        let permission = await this.$agent.p_mfa('ws_db_permission', 'create', [this.id, {
-            name: perm.name
-        }])
-        perm.id = permission.id
-        this.permissions.push(permission)
+    onChange: async function(perm, checked) {
+      if (checked) {
+        await this.$agent.p_mfa('ws_db_permission', 'set', [this.id, perm])
+        this.effective[perm] = true
       }
       else {
-        await this.$agent.p_mfa('ws_db_permission', 'delete', [perm.id])
-        let id = this.permissions.findIndex(Obj => Obj.id === perm.id)
-        if (id >= 0) {
-          this.permissions.splice(id, 1)
-        }
+        await this.$agent.p_mfa('ws_db_permission', 'unset', [this.id, perm])
+        delete this.effective[perm]
       }
-    },
-    canShow(index) {
-      if(this.ui == 'supervisor') {
-        let main_perm = 14+this.groups.length + this.queues.length
-        let records_perm = 24+this.groups.length + this.queues.length
-        if ( !this.perms_check[0].value && index >= main_perm && index < records_perm)
-          return false
-        else if ( !this.perms_check[1].value && index >= 4 && index < main_perm)
-          return false
-        else if ( !this.perms_check[2].value && index >= records_perm )
-          return false
-        else
-          return true
-      }
-    },
-    selectPerm: async function(perm) {
-      let permission = await this.$agent.p_mfa('ws_db_permission', 'create', [this.id, {
-        name: perm.name
-      }])
-      perm.id = permission.id
-      perm.value = true
-      this.permissions.push(permission)
-    },
-    selectAll() {
-      this.perms_check.forEach( (perm) => {
-        if(!perm.value)
-          this.selectPerm(perm)
-      })
     }
   },
   created () {
