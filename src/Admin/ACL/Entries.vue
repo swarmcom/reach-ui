65147363e8b8547322629839793a7e14e3d07174
martin harcar <martin.harcar@ezuce.com>
2018-02-13 15:34:56 +0100

    Add line in and line out to access list

--- src/Admin/ACL/Entries.vue
+++ src/Admin/ACL/Entries.vue
@@ -10,6 +10,12 @@
   <b-row style="margin-top: 20px"><b-col><h3>Agents:</h3></b-col></b-row>
   <b-row><b-col><agents v-model="agents" @add="set('agent', $event)" @remove="unset('agent', $event)"></agents></b-col></b-row>
 
+  <b-row style="margin-top: 20px"><b-col><h3>Lines In:</h3></b-col></b-row>
+  <b-row><b-col><line_ins v-model="line_ins" @add="set('line_in', $event)" @remove="unset('line_in', $event)"></line_ins></b-col></b-row>
+
+  <b-row style="margin-top: 20px"><b-col><h3>Lines Out:</h3></b-col></b-row>
+  <b-row><b-col><line_outs v-model="line_outs" @add="set('line_out', $event)" @remove="unset('line_out', $event)"></line_outs></b-col></b-row>
+
 </div>
 </template>
 
@@ -18,6 +24,8 @@ import Common from '@/Admin/Common'
 import Queues from '@/Admin/ACL/Queues'
 import Clients from '@/Admin/ACL/Clients'
 import Agents from '@/Admin/ACL/Agents'
+import LineIns from '@/Admin/ACL/LineIns'
+import LineOuts from '@/Admin/ACL/LineOuts'
 
 export default {
   name: 'admin-agent-group-acl',
@@ -25,7 +33,9 @@ export default {
   components: {
     'queues': Queues,
     'clients': Clients,
-    'agents': Agents
+    'agents': Agents,
+    'line_ins': LineIns,
+    'line_outs': LineOuts
   },
   props: ['id'],
   data () {
@@ -34,7 +44,9 @@ export default {
       module: 'ws_db_acl',
       queues: [],
       clients: [],
-      agents: []
+      agents: [],
+      line_ins: [],
+      line_outs: []
     }
   },
   methods: {
@@ -44,6 +56,8 @@ export default {
         this.queues = this.acl.queue? this.acl.queue : []
         this.clients = this.acl.client? this.acl.client : []
         this.agents = this.acl.agent? this.acl.agent : []
+        this.line_ins = this.acl.line_in? this.acl.line_in : []
+        this.line_outs = this.acl.line_out? this.acl.line_out : []
       }
     },
     onCancel () {
---
+++ src/Admin/ACL/LineIns.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="form-inline">
+    <autocomplete v-model="obj" :query="query" placeholder="Line In..."></autocomplete>
+    <button class="btn btn-sm btn-outline-primary" style="margin-left: 10px" v-for="obj in selected" @click="remove(obj)">{{obj.name}}</button>
+  </div>
+</template>
+
+<script>
+  import Autocomplete from '@/Widget/Autocomplete'
+  import API from '@/Admin/ACL/API'
+
+  export default {
+    name: 'acl-widget-line-ins',
+    components: {Autocomplete},
+    props: ['value'],
+    mixins: [API],
+    data () {
+      return {
+        module: 'ws_db_line_in',
+        obj: undefined,
+        selected: []
+      }
+    }
+  }
+</script>
