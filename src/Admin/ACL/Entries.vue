6d30c723492ddae213d77a22311c147e47aa01d4
martin harcar <martin.harcar@ezuce.com>
2018-02-14 13:34:03 +0100

    Add queue group to ACL

--- src/Admin/ACL/Entries.vue
+++ src/Admin/ACL/Entries.vue
@@ -3,12 +3,18 @@
   <b-row style="margin-bottom: 10px"><b-col><h3>Access Lists:</h3></b-col></b-row>
   <b-row><b-col><h3>Queues:</h3></b-col></b-row>
   <b-row><b-col><queues v-model="queues" @add="set('queue', $event)" @remove="unset('queue', $event)"></queues></b-col></b-row>
-  
+
+  <b-row style="margin-top: 20px"><b-col><h3>Queue Group:</h3></b-col></b-row>
+  <b-row><b-col><queue-groups v-model="queue_groups" @add="set('queue_group', $event)" @remove="unset('queue_group', $event)"></queue-groups></b-col></b-row>
+
   <b-row style="margin-top: 20px"><b-col><h3>Clients:</h3></b-col></b-row>
   <b-row><b-col><clients v-model="clients" @add="set('client', $event)" @remove="unset('clients', $event)"></clients></b-col></b-row>
 
   <b-row style="margin-top: 20px"><b-col><h3>Agents:</h3></b-col></b-row>
   <b-row><b-col><agents v-model="agents" @add="set('agent', $event)" @remove="unset('agent', $event)"></agents></b-col></b-row>
+
+  <b-row style="margin-top: 20px"><b-col><h3>Agent Group:</h3></b-col></b-row>
+  <b-row><b-col><agent-groups v-model="agent_groups" @add="set('agent_group', $event)" @remove="unset('agent_group', $event)"></agent-groups></b-col></b-row>
 
   <b-row style="margin-top: 20px"><b-col><h3>Lines In:</h3></b-col></b-row>
   <b-row><b-col><line-ins v-model="line_ins" @add="set('line_in', $event)" @remove="unset('line_in', $event)"></line-ins></b-col></b-row>
@@ -16,8 +22,7 @@
   <b-row style="margin-top: 20px"><b-col><h3>Lines Out:</h3></b-col></b-row>
   <b-row><b-col><line-outs v-model="line_outs" @add="set('line_out', $event)" @remove="unset('line_out', $event)"></line-outs></b-col></b-row>
 
-  <b-row style="margin-top: 20px"><b-col><h3>Agent Group:</h3></b-col></b-row>
-  <b-row><b-col><agent-groups v-model="agent_groups" @add="set('agent_group', $event)" @remove="unset('agent_group', $event)"></agent-groups></b-col></b-row>
+
 
 </div>
 </template>
@@ -26,6 +31,7 @@
 import Common from '@/Admin/Common'
 import Queues from '@/Admin/ACL/Queues'
 import Clients from '@/Admin/ACL/Clients'
+import QueueGroups from '@/Admin/ACL/QueueGroups'
 import Agents from '@/Admin/ACL/Agents'
 import AgentGroups from '@/Admin/ACL/AgentGroups'
 import LineIns from '@/Admin/ACL/LineIns'
@@ -36,6 +42,7 @@ export default {
   mixins: [Common],
   components: {
     'queues': Queues,
+    'queue-groups': QueueGroups,
     'clients': Clients,
     'agents': Agents,
     'agent-groups': AgentGroups,
@@ -52,7 +59,8 @@ export default {
       agents: [],
       line_ins: [],
       line_outs: [],
-      agent_groups: []
+      agent_groups: [],
+      queue_groups: []
     }
   },
   methods: {
@@ -64,7 +72,8 @@ export default {
         this.agents = this.acl.agent? this.acl.agent : []
         this.line_ins = this.acl.line_in? this.acl.line_in : []
         this.line_outs = this.acl.line_out? this.acl.line_out : []
-        this.agent_groups = this.acl.agent_groups? this.acl.agent_groups : []
+        this.agent_groups = this.acl.agent_group? this.acl.agent_group : []
+        this.queue_groups = this.acl.queue_group? this.acl.queue_group : []
       }
     },
     onCancel () {
---
+++ src/Admin/ACL/QueueGroups.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="form-inline">
+  <autocomplete v-model="obj" :query="query" placeholder="Queue group..."></autocomplete>
+  <button class="btn btn-sm btn-outline-primary" style="margin-left: 10px" v-for="obj in selected" :key="obj.id" @click="remove(obj)">{{obj.name}}</button>
+</div>
+</template>
+
+<script>
+import Autocomplete from '@/Widget/Autocomplete'
+import API from '@/Admin/ACL/API'
+
+export default {
+  name: 'acl-widget-queue-groups',
+  components: {Autocomplete},
+  props: ['value'],
+  mixins: [API],
+  data () {
+    return {
+      module: 'ws_db_queue_group',
+      obj: undefined,
+      selected: []
+    }
+  }
+}
+</script>
