62f05a51adf1e39b6b1220d5ffc2bb940244c913
martin harcar <martin.harcar@ezuce.com>
2018-03-05 15:42:08 +0100

    Fix increasing of memory

--- src/main.js
+++ src/main.js
@@ -31,16 +31,11 @@ Vue.directive('access', {
     let Rights = vnode.context.$agent.vm.agent.permissions
     if (! Rights[binding.arg]) {
       
-      let comment = document.createComment(' ');
+      const comment = document.createComment(' ');
       Object.defineProperty(comment, 'setAttribute', {
         value: () => undefined,
       });
       vnode.elm = comment;
-      vnode.text = ' ';
-      vnode.isComment = true;
-      vnode.context = undefined;
-      vnode.tag = undefined;
-      vnode.data.directives = undefined;
 
       if (vnode.componentInstance) {
         vnode.componentInstance.$el = comment;
