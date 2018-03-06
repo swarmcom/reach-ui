41cabb80bb26b0d32be0efc309e0bd829ee7791b
martin harcar <martin.harcar@ezuce.com>
2018-03-06 10:37:06 +0100

    Allow to make oiutbound call from list of lines defined in acl and to default line out

--- src/Agent/Dialer.vue
+++ src/Agent/Dialer.vue
@@ -1,11 +1,12 @@
 <template>
-<b-form-group v-if="this.$agent.can_call()" label="Place a call to:">
+<b-form-group v-if="can_call()" label="Place a call to:">
   <b-input-group>
     <b-form-input v-model="number" type="text" placeholder="enter a number..."></b-form-input>
     <b-input-group-button>
-      <b-dropdown text="Call as" variant="outline-secondary" right>
-        <b-dropdown-item v-for="line of this.$agent.vm.agent.lines" :key="line.id" @click="call(line)">{{line.name}}</b-dropdown-item>
+      <b-dropdown v-if="lines.length > 0" text="Call as" variant="outline-secondary" right>
+        <b-dropdown-item v-for="line of lines" :key="line.id" @click="call(line)">{{line.name}}</b-dropdown-item>
       </b-dropdown>
+      <b-btn v-if="$agent.vm.agent.line_id" @click="call($agent.vm.agent.line)" variant="outline-secondary">{{$agent.vm.agent.line.name}}</b-btn>
     </b-input-group-button>
   </b-input-group>
 </b-form-group>
@@ -14,19 +15,27 @@
 <script>
 export default {
   name: 'dialer',
-  props: ["lines"],
   data () {
     return {
       number: '',
-      line_out: {}
+      line_out: {},
+      lines: [],
     }
   },
   methods: {
+    query: async function () {
+      this.lines = await this.$agent.p_mfa('ws_agent', 'lines_out')
+    },
     call (line) {
       this.$agent.place_call(line.id, this.number)
+    },
+    can_call() {
+      return this.$agent.can_call() && (this.lines.length > 0 || this.$agent.vm.agent.line_id) 
     }
+
   },
   created () {
+    this.query()
   }
 }
 </script>
