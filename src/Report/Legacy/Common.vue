be0deb11d4eed65675189880248deee08b564b81
Pavel Farkas <pavel.farkas@ezuce.com>
2018-03-09 21:22:58 +0100

    Sort by "Abandoned %" in Reports does not work #541

---
+++ src/Report/Legacy/Common.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  methods: {
+    sortCompareCustom(a, b, key) {
+      switch(key) {
+      case 'abandoned_percent':
+        let v1 = (a.abandoned === undefined) ? -1 : a.abandoned
+        let v2 = (b.abandoned === undefined) ? -1 : b.abandoned
+        return v1 < v2 ? -1 : (v1 > v2 ? 1 : 0)
+      default:
+        return null
+      }
+    }
+  }
+}
+</script>
