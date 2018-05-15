edca9d531fd7820a259f8e77b96679af2352d77f
Roman Galeev <roman.galeev@ezuce.com>
2018-05-15 20:55:51 +0200

    Add inbound selectors

--- src/Live/Inbounds.vue
+++ src/Live/Inbounds.vue
@@ -1,11 +1,31 @@
 <template>
 <div>
   <div class="row">
-    <div class="col"><h3>Live inbound calls for {{type}}</h3></div>
+    <div class="col"><h3>Live inbound calls</h3></div>
   </div>
   <b-row style="margin-bottom: 10px">
     <b-col cols=3>
       <b-form-select v-model="type" :options="types" />
+    </b-col>
+  </b-row>
+  <b-row v-if="widget('queues')" style="margin-bottom: 10px">
+    <b-col>
+      <widget-queues v-model="queues"></widget-queues>
+    </b-col>
+  </b-row>
+  <b-row v-if="widget('queue_groups')" style="margin-bottom: 10px">
+    <b-col>
+      <widget-queue-groups v-model="queue_groups"></widget-queue-groups>
+    </b-col>
+  </b-row>
+  <b-row v-if="widget('clients')" style="margin-bottom: 10px">
+    <b-col>
+      <widget-clients v-model="clients"></widget-clients>
+    </b-col>
+  </b-row>
+  <b-row v-if="widget('line_ins')" style="margin-bottom: 10px">
+    <b-col>
+      <widget-line-ins v-model="line_ins"></widget-line-ins>
     </b-col>
   </b-row>
   <b-table style="margin-top:10px" small striped hover :items="data" :fields="fields">
@@ -37,13 +57,27 @@
 
 <script>
 import Base from '@/Live/Base'
+import Queues from '@/Report/Widget/Queues'
+import QueueGroups from '@/Report/Widget/QueueGroups'
+import Clients from '@/Report/Widget/Clients'
+import LineIns from '@/Report/Widget/LineIns'
 
 export default {
   mixins: [Base],
+  components: {
+    'widget-queues': Queues,
+    'widget-queue-groups': QueueGroups,
+    'widget-clients': Clients,
+    'widget-line-ins': LineIns
+  },
   data () {
     return {
       type: 'skills',
-      types: ['skills'],
+      types: ['skills', 'queues', 'queue_groups', 'clients', 'line_ins'],
+      queues: [],
+      queue_groups: [],
+      clients: [],
+      line_ins: [],
       fields: {
         state: { label: 'State' },
         time: { label: 'Time' },
@@ -68,8 +102,8 @@ export default {
       }
     },
     query: async function (type) {
-      this.data = await this.$agent.p_mfa('ws_live', 'inbounds', [type])
-      await this.$agent.p_mfa('ws_live', 'subscribe', ['inbounds', type])
+      this.data = await this.$agent.p_mfa('ws_live', 'inbounds', [type, this.widget_args(type)])
+      await this.$agent.p_mfa('ws_live', 'subscribe', ['inbounds', type, this.widget_args(type)])
     },
     onTimer () {
       this.data.forEach((E, i, Arr) => { 
@@ -89,6 +123,18 @@ export default {
     },
     hangup ({record, uuid}) {
       this.$agent.p_mfa('ws_supervisor', 'hangup', [record, uuid])
+    },
+    widget (type) {
+      return type == this.type
+    },
+    widget_args (type) {
+      switch (type) {
+        case 'queues': return this.queues
+        case 'queue_groups': return this.queue_groups
+        case 'clients': return this.clients
+        case 'line_ins': return this.line_ins
+        default: return []
+      }
     }
   },
   created () {
@@ -104,7 +150,7 @@ export default {
         this.skip_load = false
         return
       }
-      await this.$agent.p_mfa('ws_live', 'unsubscribe', ['inbounds', old])
+      await this.$agent.p_mfa('ws_live', 'unsubscribe', ['inbounds', old, this.widget_args(old)])
       this.query(value)
     }
   }
