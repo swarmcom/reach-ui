fa8a6fa3cd71ee15dc3d97ef38bf93662d5ab6bb
mharcar <martin.harcar@ezuce.com>
2018-11-14 15:24:37 +0100

    Make development mode running fast using cache option

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack')
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'none',
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
   entry: {
     build: [
       'babel-polyfill',
@@ -55,19 +55,9 @@ module.exports = {
         '@': path.resolve(__dirname, 'src/')
       }
   },
-  optimization: {
-    minimize: true,
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          name: "vendor",
-          minChunks: Infinity
-        }
-      }
-    }
-  },
-  devServer: { historyApiFallback: true, noInfo: true  },
+  devServer: { historyApiFallback: true, noInfo: false },
   performance: { hints: false },
+  cache: true,
   devtool: '#eval-source-map',
   plugins: [
     new CopyWebpackPlugin([{
@@ -86,11 +76,21 @@ if (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: '"production"'
+    new webpack.LoaderOptionsPlugin({ 
+      minimize: true,
+      optimization: {
+        options: {
+          minimize: true,
+          splitChunks: {
+            cacheGroups: {
+              vendor: {
+                name: "vendor",
+                minChunks: Infinity
+              }
+            }
+          }
+        }
       }
-    }),
-    new webpack.LoaderOptionsPlugin({ minimize: true })
+    })
   ])
 }
