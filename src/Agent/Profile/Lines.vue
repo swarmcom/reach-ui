436cbf0f1b8dbc3541201cf015b650d3670ec574
mharcar <martin.harcar@ezuce.com>
2019-05-06 12:40:53 +0200

    Fix console error in profile page

--- src/Agent/Profile/Lines.vue
+++ src/Agent/Profile/Lines.vue
@@ -72,12 +72,12 @@ export default {
       default: ''
     },
     value: {
-      type: String,
-      default: ''
+      type: [String, Number],
+      default: ""
     },
     effective: {
-      type: Boolean,
-      default: false
+      type: [String, Number],
+      default: ""
     }
   },
   data () {
