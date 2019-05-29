e0707594c7c47226694a46858e79cba8340a1638
mharcar <martin.harcar@ezuce.com>
2019-05-29 10:14:27 +0200

    REACH-73 export csv for agent events report

--- src/Report/Agents/Events.vue
+++ src/Report/Agents/Events.vue
@@ -11,8 +11,25 @@
       enable="range:agents:agent_groups:skills"
       @reset="reset"
     />
+    <b-row style="margin-top: 20px">
+      <b-col
+        class="cvs-download"
+        title="export to csv"
+      >
+        <download-csv
+          :data="comp_agents_events"
+          :labels="json_agents_events_labels"
+          name="agents_events.csv"
+        >
+          <icon
+            style="color:#838383"
+            name="download"
+            scale="1"
+          />
+        </download-csv>
+      </b-col>
+    </b-row>
     <b-table
-      style="margin-top: 20px"
       small
       striped
       hover
@@ -56,6 +73,32 @@ export default {
         time: { label: 'Duration', formatter: this.durationFormatter },
         release: { label: 'Release', formatter: this.nameFormatter }
       },
+      json_agents_events_labels: {
+        ts_ms: "Time",
+        agent: "Name",
+        agent_group: "Group",
+        state_from: "From",
+        state: "To",
+        time: "Duration",
+        release: "Release"
+      }
+    }
+  },
+  computed: {
+    comp_agents_events: function () {
+      let array = []
+      this.data.forEach( item => {
+        let object = {}
+        object['ts_ms'] = this.tsMsFormatter(item['ts_ms'])
+        object['agent'] = this.nameFormatter(item['agent'])
+        object['agent_group'] = this.nameFormatter(item['agent_group'])
+        object['state_from'] = item['state_from']
+        object['state'] = item['state']
+        object['time'] = this.durationFormatter(item['time'])
+        object['release'] = this.nameFormatter(item['release'])
+        array.push(object)
+      })
+      return array
     }
   },
   created () {
