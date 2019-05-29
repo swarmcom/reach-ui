32d773243f06c5818e7729f2ab7169e54674654f
mharcar <martin.harcar@ezuce.com>
2019-05-29 11:26:40 +0200

    REACH-73 export csv for agents events report and add back button

--- src/Report/Agent/Session/Events.vue
+++ src/Report/Agent/Session/Events.vue
@@ -1,18 +1,38 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col">
+    <b-row>
+      <b-col>
         <h3>Agent session events</h3>
-      </div>
-    </div>
+      </b-col>
+    </b-row>
+    <b-row style="margin-top: 20px">
+      <b-col
+        class="cvs-download"
+        title="export to csv"
+      >
+        <download-csv
+          :data="comp_agents_session_events"
+          :labels="json_agents_session_events_labels"
+          name="agents_session_events.csv"
+        >
+          <icon
+            style="color:#838383"
+            name="download"
+            scale="1"
+          />
+        </download-csv>
+      </b-col>
+    </b-row>
     <b-table
-      style="margin-top:10px"
       small
       striped
       hover
       :items="data"
       :fields="fields"
     />
+    <b-btn @click="$router.go(-1)">
+      Back
+    </b-btn>
   </div>
 </template>
 
@@ -31,11 +51,31 @@ export default {
     return {
       data: [],
       fields: {
-        ts_ms: { label: 'Time', sortable: true, formatter: this.tsFormatter },
+        ts_ms: { label: 'Time', sortable: true, formatter: this.tsMsFormatter },
         state_from: { label: 'From' },
         state: { label: 'To' },
         time: { label: 'Duration', formatter: this.durationFormatter }
+      },
+      json_agents_session_events_labels: {
+        ts_ms: "Time",
+        state_from: "From",
+        state: "To",
+        time: "Duration"
       }
+    }
+  },
+  computed: {
+    comp_agents_session_events: function () {
+      let array = []
+      this.data.forEach( item => {
+        let object = {}
+        object['ts_ms'] = this.tsMsFormatter(item['ts_ms'])
+        object['state_from'] = item['state_from']
+        object['state'] = item['state']
+        object['time'] = this.durationFormatter(item['time'])
+        array.push(object)
+      })
+      return array
     }
   },
   created () {
--- src/Report/Agents/Events.vue
+++ src/Report/Agents/Events.vue
@@ -39,6 +39,11 @@
       @row-clicked="click"
     />
     <b-row>
+      <b-col v-if="!is_standalone()">
+        <b-btn @click="$router.go(-1)">
+          Back
+        </b-btn>
+      </b-col>
       <b-col>
         <b-button
           variant="outline-primary"
