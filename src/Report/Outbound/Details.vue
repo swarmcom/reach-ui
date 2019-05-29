3bf35f1d9c4325857e77fbffda6d19e12d501d04
mharcar <martin.harcar@ezuce.com>
2019-05-29 12:47:44 +0200

    REACH-73 export csv for outbound reports and add back button

--- src/Report/Outbound/Details.vue
+++ src/Report/Outbound/Details.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
-    <div class="row">
-      <div class="col">
+    <b-row>
+      <b-col>
         <h3>Outbound details</h3>
-      </div>
-    </div>
+      </b-col>
+    </b-row>
     <widget-query
       v-model="query_params"
-      enable="step:empty_intervals"
+      enable="range:step:empty_intervals"
       @reset="reset"
     />
+    <b-row style="margin-top: 20px">
+      <b-col
+        class="cvs-download"
+        title="export to csv"
+      >
+        <download-csv
+          :data="comp_outbound_details"
+          :labels="json_outbound_details_labels"
+          name="outbound_details.csv"
+        >
+          <icon
+            style="color:#838383"
+            name="download"
+            scale="1"
+          />
+        </download-csv>
+      </b-col>
+    </b-row>
     <b-table
-      style="margin-top: 20px"
       small
       striped
       hover
@@ -21,16 +38,19 @@
       <template
         slot="answers"
         slot-scope="dataSlot"
-      >
+      > 
         {{ dataSlot.item.answers }} / {{ percentageFormatter(dataSlot.item.answers, dataSlot.item.rings) }}
       </template>
       <template
         slot="abandons"
         slot-scope="dataSlot"
       >
-        {{ dataSlot.item.abandons }} / {{ percentageFormatter(dataSlot.item.abandons, data.item.rings) }}
+        {{ dataSlot.item.abandons }} / {{ percentageFormatter(dataSlot.item.abandons, dataSlot.item.rings) }}
       </template>
     </b-table>
+    <b-btn @click="$router.go(-1)">
+      Back
+    </b-btn>
   </div>
 </template>
 
@@ -60,6 +80,32 @@ export default {
         avg_talk_time: { label: 'Average Talk Time', formatter: this.durationFormatter },
         total_talk_time: { label: 'Total Talk Time', formatter: this.durationFormatter },
       },
+      json_outbound_details_labels: {
+        ts_from: "From",
+        ts_to: "To",
+        rings: "Calls",
+        answers: "Answered ",
+        abandons: "Abandons",
+        avg_talk_time: "Average Talk Time",
+        total_talk_time: "Total Talk Time"
+      }
+    }
+  },
+  computed: {
+    comp_outbound_details: function () {
+      let array = []
+      this.data.forEach( item => {
+        let object = {}
+        object['ts_from'] = this.tsFormatter(item['ts_from'])
+        object['ts_to'] = this.tsFormatter(item['ts_to'])
+        object['rings'] = item['rings']
+        object['answers'] = item['answers'] + '/' + this.percentageFormatter(item['answers'], item['rings'])
+        object['abandons'] = item['abandons'] + '/' + this.percentageFormatter(item['abandons'], item['rings'])
+        object['avg_talk_time'] = this.durationFormatter(item['avg_talk_time'])
+        object['total_talk_time'] = this.durationFormatter(item['total_talk_time'])
+        array.push(object)
+      })
+      return array
     }
   },
   created () {
